1db662d4a4cb135db2efa4cd1f85ff16ccd98f80
Wayne <wayne@puppetlabs.com>
2014-05-13 10:59:05 -0700

    Introduce initial debian-7 acceptance test support
    Includes adding new Beaker configs for jenkins/vspher and pre_suite setup
    support.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- acceptance/suites/pre_suite/05_standard_repos.rb
+++ acceptance/suites/pre_suite/05_standard_repos.rb
@@ -1,8 +1,15 @@
 def initialize_repos_on_host(host)
+  repo_config = ENV['JVMPUPPET_REPO_CONFIG']
   if host['platform'].include? 'el-6'
     on host, "rpm -ivh https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm"
     on host, "sed -i.bak -e '/\\[puppetlabs-devel\\]/,/^$/ s|enabled=0|enabled=1|' /etc/yum.repos.d/puppetlabs.repo"
-    on host, "curl #{ENV['JVMPUPPET_REPO_CONFIG']} > /etc/yum.repos.d/jvmpuppet-puppetlabs.repo"
+    on host, "curl #{repo_config} > /etc/yum.repos.d/jvmpuppet-puppetlabs.repo"
+  elsif host['platform'].include? 'debian'
+    platform_name = ENV['PLATFORM_NAME']
+    on host, "wget -O /tmp/puppet.deb https://apt.puppetlabs.com/puppetlabs-release-#{platform_name}.deb"
+    on host, "dpkg -i /tmp/puppet.deb"
+    on host, "wget -O /etc/apt/sources.list.d/jvm-puppet-#{platform_name}.list #{repo_config}"
+    on host, "apt-get update"
   else
     raise ArgumentError, "Unsupported OS '#{host['platform']}'"
   end
