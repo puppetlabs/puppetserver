8518cf368c41bb4ac6807d2da28d08c97ce093ee
Wayne <wayne@puppetlabs.com>
2014-05-16 05:38:55 -0700

    Rearrange pre_suite code into helper.rb
    Inspired by classifier's helper.rb
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- acceptance/suites/pre_suite/70_install_puppet.rb
+++ acceptance/suites/pre_suite/70_install_puppet.rb
@@ -1,13 +1,8 @@
+step "Install MRI Puppet Agents."
+  puppet_version = ENV["PUPPET_VERSION"]
+  hosts.each do |host|
+    custom_install_puppet_package host, 'puppet', puppet_version, true
+  end
+
 step "Install JVM Puppet Master."
-  if master['platform'].include? 'debian'
-    on master, 'apt-get install --force-yes -y jvm-puppet'
-  else
-    install_package master, 'jvm-puppet'
-  end
-
-
-step "Install Puppet Agents."
-  agents.each do |agent|
-    install_package agent, 'puppet'
-  end
-
+  custom_install_puppet_package master, 'jvm-puppet' 
