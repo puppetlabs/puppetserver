37f6226f96240f874dfc919ffa2a9140df4d0f63
Wayne <wayne@puppetlabs.com>
2014-05-28 03:31:53 -0700

    (PE-4292) Address PR comments.
    * Use ezbake.rb to load EZBake::Config
    * Use EZBake::Config to perform preinst tasks.
    * Use EZBake::Config to get some jvm-puppet dependencies.
    * Remove unnecessary os_families method.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

---
+++ acceptance/suites/pre_suite/40_install_jvmpuppet_deps.rb
@@ -0,0 +1,11 @@
+case test_config[:jvmpuppet_install_type]
+when :git # only if installing from source
+  step "Install JVM Puppet Dependencies"
+    project_version = test_config[:jvmpuppet_version] ||
+      `lein with-profile acceptance pprint :version | cut -d\\" -f2`
+    ezbake_stage 'jvm-puppet', project_version
+    install_ezbake_deps_on master
+
+    # now install other deps
+    install_package master, 'java-1.7.0-openjdk'
+end
