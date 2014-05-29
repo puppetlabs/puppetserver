83d69bdbeb489b9a341c836358c369746c989958
Wayne <wayne@puppetlabs.com>
2014-05-28 17:54:43 -0700

    (PE-4292) Update install_from_ezbake.
    Moves behavior from install_from_ezbake to ezbake's template/global/Makefile.erb
    so the install_from_ezbake method is more resilient to ezbake changes.
    
    Removes install_ezbake_deps_on.
    
    Uses `rake pl:package_build_params[ref]` instead of regex matching output from
    ezbake's `lein run stage...`
    
    Rearrange pre_suite/30_install_release_repos.rb to only install JVM Puppet
    release repository if install from :package is true.
    
    After make-instal-jvmpuppet, /var/lib/puppet/state and /var/lib/puppet/reports
    are owned by root.root for some reason. They need to be owned by puppet.puppet.
    The chown command at the end of preinst fixes this.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- acceptance/suites/pre_suite/30_install_release_repos.rb
+++ acceptance/suites/pre_suite/30_install_release_repos.rb
@@ -1,10 +1,11 @@
-step "Setup JVM Puppet repositories." do
-  hosts.each do |host|
+case test_config[:jvmpuppet_install_type]
+when :package
+  step "Setup JVM Puppet repositories." do
     package_build_version = ENV['PACKAGE_BUILD_VERSION']
     if package_build_version
-      install_dev_repos_on 'jvm-puppet', host, package_build_version, "repo_configs"
+      install_dev_repos_on 'jvm-puppet', master, package_build_version, "repo_configs"
     else
-      abort("Missing required PACKAGE_BUILD_VERSION!")
+      abort("Environment variable PACKAGE_BUILD_VERSION required for package installs!")
     end
   end
 end
