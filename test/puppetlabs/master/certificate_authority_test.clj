82b42983f396ab18f83b1d43fbba4d5ab2eadd07
Nate Wolfe <nwolfe@puppetlabs.com>
2014-06-03 13:53:45 -0700

    (PE-3826) Implement naive autosign
    This commit modifies the CA service to honor Puppet's 'autosign' setting,
    but only for boolean values (whitelist & executable files coming soon).
    If the value of autosign is true, the CSR will be signed and the certificate written to disk.
    Note this is the current behavior of our CA, which basically assumes autosign is always true.
    If autosign is false, the CSR will be written to disk instead.

--- test/puppetlabs/master/certificate_authority_test.clj
+++ test/puppetlabs/master/certificate_authority_test.clj
@@ -37,6 +37,27 @@
   (testing "returns nil when certificate request not found for subject"
     (is (nil? (get-certificate-request "not-there" csrdir)))))
 
+(deftest autosign-csr?-test
+  (testing "boolean values for 'autosign'"
+    (is (true? (autosign-csr? true)))
+    (is (true? (autosign-csr? "true")))
+    (is (false? (autosign-csr? false)))
+    (is (false? (autosign-csr? "false")))
+    (is (false? (autosign-csr? "/foo/bar/autosign.conf")))))
+
+(deftest save-certificate-request!-test
+  (testing "requests are saved to disk"
+    (let [csr-stream (io/input-stream (path-to-cert-request csrdir "test-agent"))
+          path       (path-to-cert-request csrdir "foo")]
+      (try
+        (is (false? (fs/exists? path)))
+        (save-certificate-request! "foo" csr-stream csrdir)
+        (is (true? (fs/exists? path)))
+        (is (= (get-certificate-request csrdir "foo")
+               (get-certificate-request csrdir "test-agent")))
+        (finally
+          (fs/delete path))))))
+
 (deftest autosign-certificate-request!-test
   (let [subject            "test-agent"
         request-stream     (-> csrdir
