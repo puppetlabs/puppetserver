c7f3bbe5ccc75d87292de5b50ab2d33c18ec606d
Wayne <wayne@puppetlabs.com>
2014-06-09 03:16:42 -0700

    (PE-4292) Fix git install type for jvm-puppet.
    Running `lein install` just prior to ezbake_stage ensures that the ezbake
    staging action will use a locally installed jvm-puppet rather than pulling from
    nexus.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- acceptance/suites/pre_suite/40_install_jvmpuppet_deps.rb
+++ acceptance/suites/pre_suite/40_install_jvmpuppet_deps.rb
@@ -1,6 +1,7 @@
 case test_config[:jvmpuppet_install_type]
 when :git # only if installing from source
   step "Install JVM Puppet Dependencies"
+    `lein install`
     project_version = test_config[:jvmpuppet_version] ||
       `lein with-profile acceptance pprint :version | cut -d\\" -f2`
     ezbake_stage 'jvm-puppet', project_version
