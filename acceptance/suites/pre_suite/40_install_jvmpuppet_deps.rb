13840c9874e0c909a36d22df25e94215a4115a64
Wayne <wayne@puppetlabs.com>
2014-06-11 18:38:05 -0700

    Fix up dependency installs on git install type.
    This commit makes openjdk-7 and puppet-common explicit dependencies for foss
    jvm-puppet. This enables `Beaker::DSL::EZBakeUtils.install_ezbake_deps` to
    correctly install all dependencies with no hard-coded dependencies in the
    acceptance test pre_suite. Does not seem to have an impact on OS package
    generation using ezbake nor installation from package.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- acceptance/suites/pre_suite/40_install_jvmpuppet_deps.rb
+++ acceptance/suites/pre_suite/40_install_jvmpuppet_deps.rb
@@ -5,8 +5,6 @@ when :git # only if installing from source
     project_version = test_config[:jvmpuppet_version] ||
       `lein with-profile acceptance pprint :version | cut -d\\" -f2`
     ezbake_stage 'jvm-puppet', project_version
-    install_ezbake_deps master
 
-    # now install other deps
-    install_package master, 'java-1.7.0-openjdk'
+    install_ezbake_deps master
 end
