607a7afd87d0169067bbd335b86c323ddf9d09a4
Wayne <wayne@puppetlabs.com>
2014-06-19 14:29:11 -0700

    (MAINT) Move common configuration to options.rb
    These options work just as well when coming from `options.rb`. Moving them here
    paves the way to use the `sqa-utils` gem which provides `genconfig` command that
    generates a hosts config file from command line arguments of the form
    `<PLATFORM>-<LAYOUT>` where `PLATFORM` could be for example el-6.cfg and
    `LAYOUT` could be 32mcd-64a where m=master, c=console, d=database, a=agent, etc.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- acceptance/config/beaker/options.rb
+++ acceptance/config/beaker/options.rb
@@ -1,5 +1,10 @@
 {
   :is_jvm_puppet => true,
   :forge_host => 'forge-aio01-petest.puppetlabs.com',
+  "service-wait" => true,
+  "service-prefix" => 'service ',
+  "service-num-retries" => 1500,
+  "puppetservice" => 'jvm-puppet',
+  "curl-retries" => 200,
 }
 
