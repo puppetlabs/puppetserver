e477b07a22b3cfead61709b40e362d7901236dc4
Justin May <justin.may@puppetlabs.com>
2014-06-25 16:14:01 -0700

    (PE-4372) Refactored how master cert extensions are generated

--- src/clj/puppetlabs/master/certificate_authority.clj
+++ src/clj/puppetlabs/master/certificate_authority.clj
@@ -1,11 +1,27 @@
 (ns puppetlabs.master.certificate-authority
-  (:import  [java.io InputStream])
+  (:import  [java.io InputStream]
+            [org.joda.time Period DateTime])
   (:require [me.raynes.fs :as fs]
             [clojure.string :as string]
             [schema.core :as schema]
             [clojure.tools.logging :as log]
             [puppetlabs.kitchensink.core :as ks]
             [puppetlabs.certificate-authority.core :as utils]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Constants
+
+(def master-oid-whitelist
+  "A list of X509 extension OIDs which are allowed to be signed into the master
+  certificate."
+  ["2.16.840.1.113730.1.13" ;; Netscape cert comment string
+   "2.5.29.35"              ;; Authority key identifier
+   "2.5.29.19"              ;; Basic constraints
+   "2.5.29.37"              ;; Extended key usage
+   "2.5.29.15"              ;; Key usage
+   "2.5.29.17"              ;; Subject alternative names
+   "2.5.29.14"              ;; Subject key identifier
+   ])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Schemas
@@ -26,7 +42,6 @@
   handling during normal Puppet operation.
   All of these are Puppet configuration settings."
   {:autosign      (schema/either String Boolean)
-   :dns-alt-names String
    :cacert        String
    :cacrl         String
    :cakey         String
@@ -38,6 +53,18 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Internal
+
+(defn generate-not-before-date []
+  "Make the not-before date set to yesterday to avoid clock skewing issues."
+  (-> (DateTime/now)
+      (.minus (Period/days 1))
+      (.toDate)))
+
+(defn generate-not-after-date []
+  "Generate a date 5 years in the future. This will be configurable soon."
+  (-> (DateTime/now)
+      (.plus (Period/years 5))
+      (.toDate)))
 
 (schema/defn settings->cadir-paths
   "Trim down the CA settings to include only paths to files and directories.
@@ -84,13 +111,25 @@
   individual dns alt names with all surrounding whitespace removed. If
   hostnames is empty or nil, then nil is returned."
   [hostnames]
-  {:pre  [(or (nil? hostnames)
-              (string? hostnames))]
-   :post [(or (nil? %)
-              (vector? %))]}
-  (let [hostnames (string/trim hostnames)]
+  {:pre  [(or nil? (string? hostnames))]
+   :post [(every? string? %)]}
+  (let [hostnames (string/trim (or hostnames ""))]
     (when (and hostnames (> (count hostnames) 0))
       (mapv #(string/trim %) (string/split hostnames #",")))))
+
+(schema/defn ^:always-validate
+  create-master-extensions-list
+  "Create a list of extensions to be added to the master certificate."
+  [cert-name   :- String
+   settings :- {schema/Keyword schema/Any}]
+  (let [dns-alt-names (conj (split-hostnames (:dns-alt-names settings))
+                            cert-name) ;; Ensure the master's main hostname is in the list
+        alt-names-ext (when (> (count dns-alt-names) 0)
+                        ;; TODO: Create a list of OID def'ns in CA lib
+                        {:oid      "2.5.29.17"
+                         :critical false
+                         :value    {:dns-name dns-alt-names}})]
+    (filter #(not (nil? %)) [alt-names-ext])))
 
 (schema/defn initialize-ca!
   "Given the CA settings, generate and write to disk all of the necessary
@@ -103,17 +142,17 @@
   (create-parent-directories! (vals (settings->cadir-paths ca-settings)))
   (-> ca-settings :csrdir fs/file ks/mkdirs!)
   (-> ca-settings :signeddir fs/file ks/mkdirs!)
-  (let [dns-alt-names (utils/generate-dns-alt-names-ext
-                        (split-hostnames (:dns-alt-names ca-settings)))
-        keypair     (utils/generate-key-pair keylength)
-        public-key  (.getPublic keypair)
+  (let [keypair     (utils/generate-key-pair keylength)
+        public-key  (utils/get-public-key keypair)
         private-key (.getPrivate keypair)
-        x500-name   (utils/generate-x500-name (:ca-name ca-settings))
-        cacert      (-> (utils/generate-certificate-request keypair x500-name
-                                                            dns-alt-names)
-                        (utils/sign-certificate-request x500-name
-                                                        (next-serial-number)
-                                                        private-key))
+        x500-name   (utils/cn (:ca-name ca-settings))
+        cacert      (utils/sign-certificate x500-name
+                                            private-key
+                                            (next-serial-number)
+                                            (generate-not-before-date)
+                                            (generate-not-after-date)
+                                            x500-name
+                                            public-key)
         cacrl       (-> cacert
                         .getIssuerX500Principal
                         (utils/generate-crl private-key))]
@@ -136,22 +175,23 @@
   (create-parent-directories! (vals ssldir-file-paths))
   (-> ssldir-file-paths :certdir fs/file ks/mkdirs!)
   (-> ssldir-file-paths :requestdir fs/file ks/mkdirs!)
-  (let [dns-alt-names (utils/generate-dns-alt-names-ext
-                        (split-hostnames (:dns-alt-names ca-settings)))
+  (let [extensions (create-master-extensions-list master-certname ca-settings)
         ca-private-key  (-> ca-settings :cakey utils/pem->private-key)
         ca-cert (-> ca-settings :cacert utils/pem->cert)
         ca-name (:ca-name ca-settings)
         keypair      (utils/generate-key-pair keylength)
-        public-key   (.getPublic keypair)
+        public-key   (utils/get-public-key keypair)
         private-key  (.getPrivate keypair)
-        x500-name    (utils/generate-x500-name master-certname)
-        ca-x500-name (utils/generate-x500-name ca-name)
-
-        hostcert     (-> (utils/generate-certificate-request keypair x500-name
-                                                             dns-alt-names)
-                         (utils/sign-certificate-request ca-x500-name
-                                                         (next-serial-number)
-                                                         ca-private-key))]
+        x500-name    (utils/cn master-certname)
+        ca-x500-name (utils/cn ca-name)
+        hostcert     (utils/sign-certificate ca-x500-name
+                                             ca-private-key
+                                             (next-serial-number)
+                                             (generate-not-before-date)
+                                             (generate-not-after-date)
+                                             x500-name
+                                             public-key
+                                             extensions)]
     (utils/key->pem! public-key (:hostpubkey ssldir-file-paths))
     (utils/key->pem! private-key (:hostprivkey ssldir-file-paths))
     (utils/cert->pem! hostcert (:hostcert ssldir-file-paths))
@@ -203,11 +243,14 @@
    :post [(nil? %)]}
   ;; TODO PE-3173 calculate cert expiration based on ca-ttl and the CSR
   ;;              issue date and pass to utils/sign-certificate-request
-  (let [signed-cert (utils/sign-certificate-request
-                      (utils/pem->csr certificate-request)
-                      (utils/generate-x500-name ca-name)
-                      (next-serial-number)
-                      (utils/pem->private-key cakey))]
+  (let [csr (utils/pem->csr certificate-request)
+        signed-cert (utils/sign-certificate (str "CN=" ca-name)
+                                            (utils/pem->private-key cakey)
+                                            (next-serial-number)
+                                            (generate-not-before-date)
+                                            (generate-not-after-date)
+                                            (str "CN=" subject)
+                                            (utils/get-public-key csr))]
     (utils/cert->pem! signed-cert (path-to-cert signeddir subject))))
 
 (defn save-certificate-request!
