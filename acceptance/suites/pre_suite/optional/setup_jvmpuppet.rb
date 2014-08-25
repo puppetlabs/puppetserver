5a6fd5c04c7aac52c837688f16897dc56ae07ebf
Wayne <wayne@puppetlabs.com>
2014-08-25 12:32:51 -0700

    (MAINT) Fix optional pre-suite step.
    It looks like this file was left in an uncertain state during the recent
    `jvm-puppet` -> `puppet-server` rename.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- acceptance/suites/pre_suite/optional/setup_jvmpuppet.rb
+++
@@ -1,6 +0,0 @@
-step "Configure Puppet Server ." 
-  if master['platform'].include? 'el-6'
-    on master, "sed -i.bak -e 's/localhost/#{master}/' /etc/puppetserver/conf.d/puppet-server.ini"
-    on master, "sed -i.bak -e 's/localhost/#{master}/' /etc/puppetserver/conf.d/webserver.ini"
-  end
-
