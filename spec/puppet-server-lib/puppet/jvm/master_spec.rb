b30f5a9d3f7c56d46b8b320f05ab491d02063f8c
Wayne <wayne@puppetlabs.com>
2014-09-05 10:24:15 -0700

    (MAINT) Set puppet submodule and deps to 3.7.0
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- spec/puppet-server-lib/puppet/jvm/master_spec.rb
+++ spec/puppet-server-lib/puppet/jvm/master_spec.rb
@@ -14,7 +14,7 @@ describe Puppet::Server::Master do
   context "puppet version" do
     it "returns the correct puppet version number" do
       master = Puppet::Server::Master.new({}, nil)
-      master.puppetVersion.should == '3.6.2'
+      master.puppetVersion.should == '3.7.0'
     end
   end
 
