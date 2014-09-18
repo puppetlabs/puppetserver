3e164a926d49db8d8d01e44bbf7075e05da1e8ec
Justin May <justin.may@puppetlabs.com>
2014-09-18 12:42:44 -0700

    (PE-5433) Updated call to (request-handler)

--- src/clj/puppetlabs/services/master/master_core.clj
+++ src/clj/puppetlabs/services/master/master_core.clj
@@ -18,36 +18,36 @@
    - ie, any route without a version in its path (eg, /v2.0/whatever) - but
    excluding the CA-related endpoints, which are handled separately by the
    CA service."
-  [request-handler environment]
+  [request-handler]
   (compojure/routes
     ; TODO there are a bunch more that we'll need to add here
     ; https://tickets.puppetlabs.com/browse/PE-3977
     (compojure/GET "/node/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/GET "/facts/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/GET "/file_content/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/GET "/file_metadatas/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/GET "/file_metadata/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/GET "/file_bucket_file/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/PUT "/file_bucket_file/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/HEAD "/file_bucket_file/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/GET "/catalog/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/POST "/catalog/*" request
-                    (request-handler environment request))
+                    (request-handler request))
     (compojure/PUT "/report/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/GET "/resource_type/*" request
-                   (request-handler environment request))
+                   (request-handler request))
     (compojure/GET "/resource_types/*" request
-                   (request-handler environment request))))
+                   (request-handler request))))
 
 (defn root-routes
   "Creates all of the compojure routes for the master."
@@ -56,7 +56,7 @@
     (compojure/context "/v2.0" request
                        (v2_0-routes request-handler))
     (compojure/context "/:environment" [environment]
-                       (legacy-routes request-handler environment))))
+                       (legacy-routes request-handler))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
