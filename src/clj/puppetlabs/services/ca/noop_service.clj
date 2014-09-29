0a6f109e2c4fb90a4f757656ceac22dfadd25282
Justin May <justin.may@puppetlabs.com>
2014-09-29 16:09:18 -0700

    (PE-6014) Created the NOOP CA service

---
+++ src/clj/puppetlabs/services/ca/noop_service.clj
@@ -0,0 +1,10 @@
+(ns puppetlabs.services.ca.noop-service
+  "Contains a NOOP version of the certificate authority service. This is
+  required to maintain service loading order on installations which are not
+  running a CA service."
+  (:require [puppetlabs.services.protocols.ca :refer [CaService]]
+            [puppetlabs.trapperkeeper.core :as tk]))
+
+(tk/defservice certificate-authority-noop-service
+  CaService)
+
