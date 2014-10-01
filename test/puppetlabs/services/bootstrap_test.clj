6dce68a8a47c154146eb511e030a2af982573364
Justin May <justin.may@puppetlabs.com>
2014-10-01 15:59:08 -0700

    (PE-6014) Removed bootstrap test

--- test/puppetlabs/services/bootstrap_test.clj
+++ test/puppetlabs/services/bootstrap_test.clj
@@ -22,20 +22,15 @@
 (def dev-bootstrap-file
   "./dev/bootstrap.cfg")
 
-(def dev-bootstrap-no-ca-file
-  "./dev/bootstrap-no-ca.cfg")
-
 (deftest test-app-startup
   (testing "Trapperkeeper can be booted successfully using the dev config files."
-    (doseq [bootstrap-file [dev-bootstrap-file
-                            dev-bootstrap-no-ca-file]]
-      (with-no-jvm-shutdown-hooks
-        (let [config (tk-config/load-config dev-config-file)
-              services (tk-bootstrap/parse-bootstrap-config! bootstrap-file)]
-          (->
-            (tk/build-app services config)
-            (tk-internal/throw-app-error-if-exists!))))
-      (is (true? true)))))
+    (with-no-jvm-shutdown-hooks
+      (let [config (tk-config/load-config dev-config-file)
+            services (tk-bootstrap/parse-bootstrap-config! dev-bootstrap-file)]
+        (->
+          (tk/build-app services config)
+          (tk-internal/throw-app-error-if-exists!))))
+    (is (true? true))))
 
 
 (defn validate-connection-failure
