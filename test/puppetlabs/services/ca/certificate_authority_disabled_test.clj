4b125cc022fdf14c895e29d4ef82632a0aae403b
Justin May <justin.may@puppetlabs.com>
2014-10-01 15:45:49 -0700

    (PE-6014) Test uses puppet.conf fixture

--- test/puppetlabs/services/ca/certificate_authority_disabled_test.clj
+++ test/puppetlabs/services/ca/certificate_authority_disabled_test.clj
@@ -9,37 +9,32 @@
             [puppetlabs.trapperkeeper.testutils.logging :as logutils]
             [puppetlabs.trapperkeeper.testutils.bootstrap :as tk-testutils]))
 
-(def ssl-dir "./target/certificate-authority-disabled-test/")
+(def puppet-conf
+  "./dev-resources/puppetlabs/services/ca/certificate_authority_dissabled_test/master/conf/puppet.conf")
+
+(use-fixtures :each (jruby-testutils/with-puppet-conf puppet-conf))
 
 (deftest ca-disabled-files-test
   (testing "Ensure no certificates are generated when CA disabled service is enabled."
-    (try
-      (fs/mkdirs ssl-dir)
+    (logutils/with-test-logging
+      (tk-testutils/with-app-with-config
+        app
 
-      (logutils/with-test-logging
-        (tk-testutils/with-app-with-config
-          app
+        [profiler/puppet-profiler-service
+         jruby/jruby-puppet-pooled-service
+         disabled/certificate-authority-disabled-service]
 
-          [profiler/puppet-profiler-service
-           jruby/jruby-puppet-pooled-service
-           disabled/certificate-authority-disabled-service]
+        (-> (jruby-testutils/jruby-puppet-tk-config
+              (jruby-testutils/jruby-puppet-config 1))
+            (assoc-in [:jruby-puppet :master-conf-dir]
+                      jruby-testutils/conf-dir))
 
-          (-> (jruby-testutils/jruby-puppet-tk-config
-                (jruby-testutils/jruby-puppet-config 1))
-              (assoc-in [:jruby-puppet :master-conf-dir]
-                        "dev-resources/puppetlabs/services/ca/certificate_authority_dissabled_test/master/conf"))
-
-          (let [jruby-service (tk-app/get-service app :JRubyPuppetService)]
-            (jruby/with-jruby-puppet
-              jruby-puppet jruby-service
-              (doseq [subdir ["certificate_requests"
-                              "certs"
-                              "private"
-                              "private_keys"
-                              "public_keys"]]
-                (is (empty? (fs/list-dir (str ssl-dir "/" subdir)))))))))
-      (finally
-        (fs/delete-dir ssl-dir)))))
+        (let [jruby-service (tk-app/get-service app :JRubyPuppetService)]
+          (jruby/with-jruby-puppet
+            jruby-puppet jruby-service
+            (let [ssl-dir (str jruby-testutils/conf-dir "/ssl")]
+              (is (not (nil? (fs/list-dir ssl-dir))))
+              (is (empty? (fs/list-dir (str ssl-dir "/ca")))))))))))
 
 
 
