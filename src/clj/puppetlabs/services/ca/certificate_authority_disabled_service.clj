502ddf569fefe61fd67ad561d768b14fdcb41bf6
Justin May <justin.may@puppetlabs.com>
2014-10-01 16:36:04 -0700

    (PE-6014) Fixed misspelling

--- src/clj/puppetlabs/services/ca/certificate_authority_disabled_service.clj
+++ src/clj/puppetlabs/services/ca/certificate_authority_disabled_service.clj
@@ -1,11 +1,11 @@
 (ns puppetlabs.services.ca.certificate-authority-disabled-service
-  "Contains a NOOP version of the certificate authority service. This is
-  required to maintain service loading order on installations which are not
-  running a CA service."
   (:require [puppetlabs.services.protocols.ca :refer [CaService]]
             [puppetlabs.trapperkeeper.core :as tk]))
 
 (tk/defservice certificate-authority-disabled-service
+               "Contains a NOOP version of the certificate authority service. This is
+                 required to maintain service loading order on installations which are not
+                 running a CA service."
   CaService
   [])
 
