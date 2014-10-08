8c24f9a67ff33ebd6be5fd04a792da5a252dd9a1
Nate Wolfe <nwolfe@puppetlabs.com>
2014-10-08 14:47:34 -0700

    (SERVER-30) Relax required files for CA fail-fast
    This commit changes the CA initialization fail-fast behavior to no
    longer require the CA public key. Directories are also no longer
    required, but will be created automatically if they don't already exist.

--- src/clj/puppetlabs/puppetserver/certificate_authority.clj
+++ src/clj/puppetlabs/puppetserver/certificate_authority.clj
@@ -124,6 +124,10 @@
   "The set of SSL files that are required on the master."
   [:hostprivkey :hostcert :localcacert])
 
+(def required-ca-files
+  "The set of SSL related files that are required on the CA."
+  [:cacert :cacrl :cakey :cert-inventory :serial])
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Internal
 
@@ -520,9 +524,9 @@
   (create-parent-directories! (vals (settings->ssldir-paths settings)))
   (-> settings :certdir fs/file ks/mkdirs!)
   (-> settings :requestdir fs/file ks/mkdirs!)
-  (let [ca-public-key  (utils/pem->public-key (:capub ca-settings))
+  (let [ca-cert        (utils/pem->cert (:cacert ca-settings))
+        ca-public-key  (.getPublicKey ca-cert)
         ca-private-key (utils/pem->private-key (:cakey ca-settings))
-        ca-cert        (utils/pem->cert (:cacert ca-settings))
         next-serial    (next-serial-number! (:serial ca-settings))
         keypair        (utils/generate-key-pair keylength)
         public-key     (utils/get-public-key keypair)
@@ -859,23 +863,34 @@
   [cacrl :- schema/Str]
   (slurp cacrl))
 
+(schema/defn ensure-directories-exist!
+  "Create any directories used by the CA if they don't already exist."
+  [settings :- CaSettings]
+  (doseq [dir [:csrdir :signeddir]]
+    (let [path (get settings dir)]
+      (when-not (fs/exists? path)
+        (ks/mkdirs! path)))))
+
 (schema/defn ^:always-validate
   initialize!
   "Given the CA configuration settings and an optional keylength,
    ensure that all required SSL files exist. If all files exist,
    new ones will not be generated. If only some are found
    (but others are missing), an exception is thrown."
-  ([ca-settings]
-     (initialize! ca-settings utils/default-key-length))
-  ([ca-settings :- CaSettings
+  ([settings]
+     (initialize! settings utils/default-key-length))
+  ([settings :- CaSettings
     keylength :- schema/Int]
-     (let [required-ca-files (vals (settings->cadir-paths ca-settings))]
-       (if (every? fs/exists? required-ca-files)
+     (ensure-directories-exist! settings)
+     (let [required-files (-> (settings->cadir-paths settings)
+                              (select-keys required-ca-files)
+                              (vals))]
+       (if (every? fs/exists? required-files)
          (log/info "CA already initialized for SSL")
          (let [{found   true
-                missing false} (group-by fs/exists? required-ca-files)]
-           (if (= required-ca-files missing)
-             (generate-ssl-files! ca-settings keylength)
+                missing false} (group-by fs/exists? required-files)]
+           (if (= required-files missing)
+             (generate-ssl-files! settings keylength)
              (throw (partial-state-error "CA" found missing))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
