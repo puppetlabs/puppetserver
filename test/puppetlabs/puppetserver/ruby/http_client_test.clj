4232f1f37a102e75d1d27688dfb66aa5356fb26c
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-10-09 13:54:57 -0700

    (SERVER-43) test coverage for ruby HTTP client
    Refactor test for extensibility and add a test case for HTTP POST.
    Use a ScriptingContainer with persistent local variables, so we can
    re-use the HttpClient instance for additional tests.

--- test/puppetlabs/puppetserver/ruby/http_client_test.clj
+++ test/puppetlabs/puppetserver/ruby/http_client_test.clj
@@ -1,4 +1,6 @@
 (ns puppetlabs.puppetserver.ruby.http-client-test
+  (:import (org.jruby.embed LocalContextScope LocalVariableBehavior
+                            ScriptingContainer))
   (:require [clojure.test :refer :all]
             [puppetlabs.trapperkeeper.testutils.webserver :as jetty9]
             [puppetlabs.trapperkeeper.testutils.webserver.common :refer [http-get]]
@@ -10,15 +12,24 @@
   {:status 200
    :body "hi"})
 
+(def scripting-container
+  "A JRuby ScriptingContainer with LocalVariableBehavior.PERSISTENT
+  so that local variables will persistent across calls to 'runScriptlet'."
+  (-> (ScriptingContainer. LocalContextScope/SINGLETHREAD
+                           LocalVariableBehavior/PERSISTENT)
+      (jruby-puppet/prep-scripting-container jruby-testutils/ruby-load-path
+                                             jruby-testutils/gem-home)))
+
 (deftest test-ruby-http-client
   (jetty9/with-test-webserver ring-app port
-    (let [sc (jruby-puppet/empty-scripting-container
-               jruby-testutils/ruby-load-path
-               jruby-testutils/gem-home)]
-      (.runScriptlet sc "require 'puppet/server/http_client'")
-      (is (= "hi" (.runScriptlet
-                    sc
-                    (format
-                      (str "c = Puppet::Server::HttpClient.new('localhost', %d, {:use_ssl => false});\n"
-                           "c.get('/', {}).body")
-                      port)))))))
+    (.runScriptlet
+      scripting-container
+      (str "require 'puppet/server/http_client';"
+           (format "c = Puppet::Server::HttpClient.new('localhost', %d, {:use_ssl => false});"
+                   port)))
+
+    (testing "HTTP GET"
+      (is (= "hi" (.runScriptlet scripting-container "c.get('/', {}).body"))))
+
+    (testing "HTTP POST"
+      (is (= "hi" (.runScriptlet scripting-container "c.post('/', 'foo', {}).body"))))))
