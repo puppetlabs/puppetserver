f916a888cd1cf709c6b72dc7f4006d801ac8d4a6
Wayne <wayne@puppetlabs.com>
2014-10-14 11:22:11 -0700

    (MAINT) Minor presuite cleanup.
    * Temporary file should use 'puppetserver' instead of 'puppetdb' in its name.
    * Restructure code so it's a little easier to read.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- acceptance/suites/pre_suite/foss/85_configure_sut.rb
+++ acceptance/suites/pre_suite/foss/85_configure_sut.rb
@@ -1,11 +1,14 @@
 step "Prevent master nodes from checking in with dujour" do
-manifest_path = master.tmpfile("puppetdb_manifest.pp")
-manifest_content = <<-EOS
-host { "updates.puppetlabs.com":
-ip => '127.0.0.1',
-ensure => 'present',
-}
-EOS
-create_remote_file(master, manifest_path, manifest_content)
-on master, puppet_apply("#{manifest_path}")
+  manifest_path = master.tmpfile("puppetserver_manifest.pp")
+
+  manifest_content = <<-EOS
+    host { "updates.puppetlabs.com":
+    ip => '127.0.0.1',
+    ensure => 'present',
+  }
+  EOS
+
+  create_remote_file(master, manifest_path, manifest_content)
+
+  on master, puppet_apply("#{manifest_path}")
 end
