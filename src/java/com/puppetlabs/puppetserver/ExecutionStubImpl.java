95fda61ff71cac04144a386b3fe305727e83bb60
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-10-14 17:00:08 -0700

    (SERVER-61) improvements to the ExecutionStub
    * The execution stub now returns an instance of
      Puppet::Util::Execution::ProcessOutput, as per the contract of
      Puppet::Util::Execution.execute.
    
    * Add basic spec/unit tests
    
    * In java, close the reader used to read the process's STDOUT.
    
    * In ruby, close the file handles passed-in for STDIN, STDOUT, and
      STDERR; we're not using these (this is implemented in java instead),
      but Puppet::Util::Execution.execute does not close them when an
      execution stub is installed.

--- src/java/com/puppetlabs/puppetserver/ExecutionStubImpl.java
+++ src/java/com/puppetlabs/puppetserver/ExecutionStubImpl.java
@@ -6,23 +6,38 @@ import java.io.InputStreamReader;
 
 public class ExecutionStubImpl {
 
-    // This is copied from
-    // http://www.mkyong.com/java/how-to-execute-shell-command-from-java/
-    public static String executeCommand(String command)
+    /**
+     * Executes the given command in a separate process.
+     *
+     * @param command the command to execute
+     * @return A 2-element array; the first element is the output of the process.
+     *      The second element is the exit code of the process.
+     *
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    public static Object[] executeCommand(String command)
             throws InterruptedException, IOException {
+
+        // This is adapted from
+        // http://www.mkyong.com/java/how-to-execute-shell-command-from-java/
 
         StringBuilder output = new StringBuilder();
 
         Process p = Runtime.getRuntime().exec(command);
         p.waitFor();
+
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(p.getInputStream()));
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
 
-        String line;
-        while ((line = reader.readLine()) != null) {
-            output.append(line).append("\n");
+            return new Object[] {output.toString(), p.exitValue()};
+        } finally {
+            reader.close();
         }
-
-        return output.toString();
     }
 }
