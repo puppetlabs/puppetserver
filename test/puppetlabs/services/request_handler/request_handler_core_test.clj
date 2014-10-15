05f5297bffb6d94310b72a791a81fd3ebc81c05e
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2014-10-15 11:56:53 -0700

    (SERVER-39) Allow a certificate to be passed into a request via X header
    This commit adds the ability for a certificate to be supplied in a web
    request as the payload of a custom HTTP header, X-Client-Cert.  The
    header will only be parsed for a value if the `allow-header-cert-info`
    master setting is set to true.  When the certificate is retrieved, it
    will be supplied as the client certificate to the Ruby request handler
    stack, as a direct replacement for a certificate object that would be
    typically be provided over SSL.

--- test/puppetlabs/services/request_handler/request_handler_core_test.clj
+++ test/puppetlabs/services/request_handler/request_handler_core_test.clj
@@ -2,7 +2,32 @@
   (:import (java.io StringReader ByteArrayInputStream))
   (:require [puppetlabs.services.request-handler.request-handler-core :as core]
             [puppetlabs.certificate-authority.core :as cert-utils]
-            [clojure.test :refer :all]))
+            [puppetlabs.puppetserver.certificate-authority :as cert-authority]
+            [clojure.test :refer :all]
+            [ring.util.codec :as ring-codec]
+            [slingshot.slingshot :as sling]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Test Data
+
+(def test-resources-dir (str "./dev-resources/puppetlabs/services/"
+                             "request_handler/request_handler_core_test"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Utilities
+
+(defmethod assert-expr 'thrown-with-slingshot? [msg form]
+  (let [expected (nth form 1)
+        body (nthnext form 2)]
+    `(sling/try+
+       ~@body
+       (do-report {:type :fail :message ~msg :expected ~expected :actual nil})
+       (catch map? actual#
+         (do-report {:type     (if (= actual# ~expected) :pass :fail)
+                     :message  ~msg
+                     :expected ~expected
+                     :actual   actual#})
+         actual#))))
 
 (defn puppet-server-config
   [allow-header-certs]
@@ -11,18 +36,26 @@
                      :ssl-client-header        "HTTP_X_CLIENT_DN"}
      :master        {:allow-header-cert-info allow-header-certs}}))
 
+(defn validate-cert-decoding-failure
+  [message request-options]
+  (is (thrown-with-slingshot?
+        {:type    :bad-request
+         :message message}
+        (core/as-jruby-request
+          (puppet-server-config true)
+          (assoc request-options :request-method :GET)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Tests
+
 (deftest get-cert-common-name-test
   (testing (str "expected common name can be extracted from the certificate on "
                 "a request")
-    (let [cert    (-> "./dev-resources/puppetlabs/services/request_handler/request_handler_core_test/localhost.pem"
-                      cert-utils/pem->certs
-                      first)
-          request {:ssl-client-cert cert}]
-      (is (= "localhost" (core/get-cert-common-name request)))))
-  (testing "nil returned for cn when no certificate on request"
-    (is (nil? (core/get-cert-common-name {}))))
+    (let [cert (cert-utils/pem->cert
+                 (str test-resources-dir "/localhost.pem"))]
+      (is (= "localhost" (core/get-cert-common-name cert)))))
   (testing "nil returned for cn when certificate on request is nil"
-    (is (nil? (core/get-cert-common-name {:ssl-client-cert nil})))))
+    (is (nil? (core/get-cert-common-name nil)))))
 
 (deftest wrap-params-for-jruby-test
   (testing "get with no query parameters returns empty params"
@@ -43,9 +76,9 @@
       (is (= "" (:body-string wrapped-request))
           "Unexpected body string in wrapped request")))
   (testing "post with form parameters returns expected values"
-    (let [body-string     "one=1&two=2%202&arr[]=3&arr[]=4"
+    (let [body-string "one=1&two=2%202&arr[]=3&arr[]=4"
           wrapped-request (core/wrap-params-for-jruby
-                            {:body (StringReader. body-string)
+                            {:body         (StringReader. body-string)
                              :content-type "application/x-www-form-urlencoded"
                              :params       {:bogus ""}})]
       (is (= {"one" "1", "two" "2 2", "arr[]" ["3" "4"]}
@@ -54,9 +87,9 @@
       (is (= body-string (:body-string wrapped-request))
           "Unexpected body string in wrapped request")))
   (testing "post with plain text in default encoding returns expected values"
-    (let [body-string     "some random text"
+    (let [body-string "some random text"
           wrapped-request (core/wrap-params-for-jruby
-                            {:body (StringReader. body-string)
+                            {:body         (StringReader. body-string)
                              :content-type "plain/text"
                              :params       {:bogus ""}})]
       (is (= {} (:params wrapped-request))
@@ -67,14 +100,14 @@
     (let [body-string-from-utf16 (String. (.getBytes
                                             "some random text from utf-16"
                                             "UTF-16"))
-          wrapped-request        (core/wrap-params-for-jruby
-                                   {:body                (ByteArrayInputStream.
-                                                           (.getBytes
-                                                             body-string-from-utf16
-                                                             "UTF-16"))
-                                    :content-type        "plain/text"
-                                    :character-encoding  "UTF-16"
-                                    :params              {:bogus ""}})]
+          wrapped-request (core/wrap-params-for-jruby
+                            {:body               (ByteArrayInputStream.
+                                                   (.getBytes
+                                                     body-string-from-utf16
+                                                     "UTF-16"))
+                             :content-type       "plain/text"
+                             :character-encoding "UTF-16"
+                             :params             {:bogus ""}})]
       (is (= {} (:params wrapped-request))
           "Unexpected params in wrapped request")
       (is (= body-string-from-utf16 (:body-string wrapped-request))
@@ -89,44 +122,118 @@
 
 (deftest cert-info-in-headers
   "In the case where Puppet Server is running under HTTP with an upstream HTTPS
-  terminator, the cert's CN and authenticated status will be provided as HTTP
-  headers."
-  (testing "providing headers but not the pupper server config won't work."
-    (let [req (core/as-jruby-request
-                (puppet-server-config false)
-                {:request-method :GET
-                 :headers        {"x-client-verify" "SUCCESS"
-                                  "x-client-dn"     "CN=puppet"}})]
-      (is (not  (get req :authenticated)))
-      (is (nil? (get req :client-cert-cn)))
-      (is (nil? (get req :client-cer)))))
+  terminator, the cert's CN, cert, and authenticated status will be provided as
+  HTTP headers.  If cert info is not provided in the headers but is available
+  via SSL, the SSL info will be used."
+  (let [single-cert-url-encoded (-> (str test-resources-dir "/localhost.pem")
+                                    slurp
+                                    ring-codec/url-encode)
+        chain-cert-url-encoded (-> (str test-resources-dir "/master-with-all-cas.pem")
+                                   slurp
+                                   ring-codec/url-encode)]
 
-  (testing "providing headers and allow-header-cert-info to true works"
-    (let [req (core/as-jruby-request
-                (puppet-server-config true)
-                {:request-method :GET
-                 :headers        {"x-client-verify" "SUCCESS"
-                                  "x-client-dn"     "CN=puppet"}})]
-      (is (get req :authenticated))
-      (is (= "puppet" (get req :client-cert-cn)))
-      (is (nil? (get req :client-cert)))))
+    (testing "providing headers but not the puppet server config won't work."
+      (let [req (core/as-jruby-request
+                  (puppet-server-config false)
+                  {:request-method :GET
+                   :headers        {"x-client-verify" "SUCCESS"
+                                    "x-client-dn"     "CN=puppet"
+                                    "x-client-cert"   single-cert-url-encoded}})]
+        (is (not (get req :authenticated)))
+        (is (nil? (get req :client-cert-cn)))
+        (is (nil? (get req :client-cert)))))
 
-  (testing "a malformed DN string fails"
-    (let [req (core/as-jruby-request
-                (puppet-server-config true)
-                {:request-method :GET
-                 :headers        {"x-client-verify" "SUCCESS"
-                                  "x-client-dn"     "invalid-dn"}})]
-      (is (not (get req :authenticated)))
-      (is (nil? (get req :client-cert)))
-      (is (nil? (get req :client-cert-cn)))))
+    (testing "providing headers and allow-header-cert-info to true works"
+      (let [req (core/as-jruby-request
+                  (puppet-server-config true)
+                  {:request-method :GET
+                   :headers        {"x-client-verify" "SUCCESS"
+                                    "x-client-dn"     "CN=puppet"
+                                    "x-client-cert"   single-cert-url-encoded}})]
+        (is (get req :authenticated))
+        (is (= "puppet" (get req :client-cert-cn)))
+        (is (= "CN=localhost"
+               (cert-authority/get-subject (get req :client-cert))))))
 
-  (testing "Setting the auth header to something other than 'SUCCESS' fails"
-    (let [req (core/as-jruby-request
-                (puppet-server-config true)
-                {:request-method :GET
-                 :headers        {"x-client-verify" "fail"
-                                  "x-client-dn"     "CN=puppet"}})]
-      (is (not (get req :authenticated)))
-      (is (= "puppet" (get req :client-cert-cn)))
-      (is (nil? (get req :client-cert))))))
+    (testing "a malformed DN string fails"
+      (let [req (core/as-jruby-request
+                  (puppet-server-config true)
+                  {:request-method :GET
+                   :headers        {"x-client-verify" "SUCCESS"
+                                    "x-client-dn"     "invalid-dn"}})]
+        (is (not (get req :authenticated)))
+        (is (nil? (get req :client-cert)))
+        (is (nil? (get req :client-cert-cn)))))
+
+    (testing "Setting the auth header to something other than 'SUCCESS' fails"
+      (let [req (core/as-jruby-request
+                  (puppet-server-config true)
+                  {:request-method :GET
+                   :headers        {"x-client-verify" "fail"
+                                    "x-client-dn"     "CN=puppet"}})]
+        (is (not (get req :authenticated)))
+        (is (= "puppet" (get req :client-cert-cn)))
+        (is (nil? (get req :client-cert)))))
+
+    (testing "first header cert from chain used when allow-header-cert-info true"
+      (let [req (core/as-jruby-request
+                  (puppet-server-config true)
+                  {:request-method :GET
+                   :headers        {"x-client-verify" "SUCCESS"
+                                    "x-client-dn"     "CN=puppet"
+                                    "x-client-cert"   chain-cert-url-encoded}})]
+        (is (= "CN=master1.example.org"
+               (cert-authority/get-subject (get req :client-cert))))))
+
+    (testing "cert from ssl used when none in header and allow-header-cert-info true"
+      (let [cert (cert-utils/pem->cert
+                   (str test-resources-dir "/localhost.pem"))
+            req (core/as-jruby-request
+                  (puppet-server-config true)
+                  {:request-method  :GET
+                   :ssl-client-cert cert
+                   :headers         {"x-client-verify" "SUCCESS"
+                                     "x-client-dn"     "CN=puppet"}})]
+        (is (identical? cert (get req :client-cert)))))
+
+    (testing "cert info from ssl used when allow-header-cert-info false"
+      (let [cert (cert-utils/pem->cert
+                   (str test-resources-dir "/localhost.pem"))
+            req (core/as-jruby-request
+                  (puppet-server-config false)
+                  {:request-method  :GET
+                   :ssl-client-cert cert
+                   :headers         {"x-client-verify" "SUCCESS"
+                                     "x-client-dn"     "CN=puppet"
+                                     "x-client-cert"   chain-cert-url-encoded}})]
+        (is (get req :authenticated))
+        (is (= "localhost" (get req :client-cert-cn)))
+        (is (identical? cert (get req :client-cert)))))))
+
+(deftest cert-decoding-failures
+  "A cert provided in the x-client-cert header that cannot be decoded into
+  an X509Certificate object throws the expected failure"
+  (testing "Improperly URL encoded content"
+    (validate-cert-decoding-failure
+      "Unable to URL decode the x-client-cert header: For input string: \"1%\""
+      {:headers {"x-client-cert" "%1%2"}}))
+  (testing "Bad certificate content"
+    (validate-cert-decoding-failure
+      "Unable to parse x-client-cert into certificate: -----END CERTIFICATE not found"
+      {:headers {"x-client-cert" "-----BEGIN%20CERTIFICATE-----%0AM"}}))
+  (testing "No certificate in content"
+    (validate-cert-decoding-failure
+      "No certs found in PEM read from x-client-cert"
+      {:headers {"x-client-cert" "NOCERTSHERE"}})))
+
+(deftest handle-request-test
+  (testing "slingshot bad requests translated to ring response"
+    (let [bad-message "it's real bad"]
+      (with-redefs [core/as-jruby-request (fn [_ _]
+                                            (core/throw-bad-request
+                                              bad-message))]
+        (let [response (core/handle-request {:body (StringReader. "blah")}
+                                            {}
+                                            {})]
+          (is (= 400 (:status response)) "Unexpected response status")
+          (is (= bad-message) "Unexpected response body"))))))
