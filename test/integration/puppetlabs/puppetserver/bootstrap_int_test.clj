407188857d922e9b6a45f0e2ed7b54e4cefbf67d
Chris Price <chris@puppetlabs.com>
2014-11-10 10:47:06 -0800

    (SERVER-112) Introduce bootstrap testutils, improve bootstrap test
    Prior to this commit, the bootstrap test wasn't actually doing
    much work.  It would create a trapperkeeper app to make sure that
    there were no failures in just loading the service *namespaces*,
    but it did not call any of the lifecycle functions of any of the
    services, so would not actually do much in terms of validating
    that the app could be run.
    
    This commit improves the test to make it actually do a full startup
    of the app based on the sample config files.
    
    It also introduces a bootstrap_testutils namespace, which contains
    some test utility code relating to booting up a puppet server
    instance during testing.  This will be useful for some forthcoming
    tests that I am writing related to testing puppet's directory
    environments.

--- test/integration/puppetlabs/puppetserver/bootstrap_int_test.clj
+++ test/integration/puppetlabs/puppetserver/bootstrap_int_test.clj
@@ -2,48 +2,13 @@
   (:import (java.io IOException)
            (org.apache.http ConnectionClosedException))
   (:require [clojure.test :refer :all]
-            [puppetlabs.kitchensink.testutils :refer [with-no-jvm-shutdown-hooks]]
-            [puppetlabs.services.jruby.jruby-testutils :as jruby-testutils]
-            [puppetlabs.trapperkeeper.core :as tk]
-            [puppetlabs.trapperkeeper.bootstrap :as tk-bootstrap]
-            [puppetlabs.trapperkeeper.internal :as tk-internal]
-            [puppetlabs.trapperkeeper.config :as tk-config]
-            [puppetlabs.trapperkeeper.testutils.bootstrap
-             :as tk-bootstrap-testutils]
-            [puppetlabs.trapperkeeper.testutils.webserver.common
-             :as tk-webserver-testutils]
-            [puppetlabs.trapperkeeper.testutils.logging :as logging]
-            [me.raynes.fs :as fs]
-            [puppetlabs.kitchensink.core :as ks]))
+            [puppetlabs.puppetserver.bootstrap-testutils :as bootstrap]
+            [puppetlabs.trapperkeeper.testutils.logging :as logging]))
 
 (use-fixtures :each logging/reset-logging-config-after-test)
 
-(def dev-config-file
-  "./dev/puppet-server.conf.sample")
-
-(def dev-bootstrap-file
-  "./dev/bootstrap.cfg")
-
 (deftest ^:integration test-app-startup
   (testing "Trapperkeeper can be booted successfully using the dev config files."
-    (let [tmp-conf (ks/temp-file "puppet-server" ".conf")]
-      (fs/copy dev-config-file tmp-conf)
-      (with-no-jvm-shutdown-hooks
-        (let [config (tk-config/load-config tmp-conf)
-              services (tk-bootstrap/parse-bootstrap-config! dev-bootstrap-file)]
-          (->
-            (tk/build-app services config)
-            (tk-internal/throw-app-error-if-exists!)))))
+    (bootstrap/with-puppetserver-running app {}
+      (is (true? true)))
     (is (true? true))))
-
-
-(defn validate-connection-failure
-  [f]
-  (try
-    (f)
-    (is false "Connection succeeded but should have failed")
-    (catch ConnectionClosedException e)
-    (catch IOException e
-      (if-not (= (.getMessage e) "Connection reset by peer")
-        (throw e))))
-  nil)
