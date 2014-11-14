a6da559917d69a6db67c0f20f0bd5e29d8202d01
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-11-14 12:03:40 -0800

    (maint) delete a couple unused functions

--- src/clj/puppetlabs/puppetserver/ringutils.clj
+++ src/clj/puppetlabs/puppetserver/ringutils.clj
@@ -1,5 +1,5 @@
 (ns puppetlabs.puppetserver.ringutils
-  (:import (clojure.lang ExceptionInfo IFn)
+  (:import (clojure.lang IFn)
            (java.security.cert X509Certificate))
   (:require [clojure.tools.logging :as log]
             [puppetlabs.kitchensink.core :as ks]
@@ -50,20 +50,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
 
-(defn log-failed-request!
-  "Logs a failed HTTP request.
-  `exception` is expected to be an instance of ExceptionInfo"
-  [req exception]
-  {:pre [(map? req)
-         (instance? ExceptionInfo exception)]}
-  (log/error "===================================================")
-  (log/error "Failed request:")
-  (log/error (ks/pprint-to-string (dissoc req :ssl-client-cert)))
-  (log/error "---------------------------------------------------")
-  (log/error (.getMessage exception))
-  (log/error (ks/pprint-to-string (dissoc (.getData exception) :environment)))
-  (log/error "==================================================="))
-
 (defn wrap-request-logging
   "A ring middleware that logs the request."
   [handler]
@@ -81,13 +67,6 @@
     (let [resp (handler req)]
       (log/trace "Computed response:" resp)
       resp)))
-
-(defn json-request?
-  "Does the given request contain JSON, according to its 'Content-Type' header?
-  This is copied directly out of ring.middleware.json"
-  [request]
-  (if-let [type (:content-type request)]
-    (not (empty? (re-find #"^application/(.+\+)?json" type)))))
 
 (schema/defn client-allowed-access? :- schema/Bool
   "Determines if the client in the request is allowed to access the
