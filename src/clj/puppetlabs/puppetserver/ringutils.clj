7e4bf8e9c8f725f114c91c6b04c677e690879338
Chris Price <chris@puppetlabs.com>
2014-11-17 14:42:16 -0800

    (SERVER-113) Refactor whitelist logic out to ringutils
    We are going to use the same whitelist logic for the new admin
    endpoint that we were using for the cert-status endpoint.  This
    commit refactors the logic out to the common ringutils namespace
    so that we can access it from the new code.

--- src/clj/puppetlabs/puppetserver/ringutils.clj
+++ src/clj/puppetlabs/puppetserver/ringutils.clj
@@ -1,7 +1,54 @@
 (ns puppetlabs.puppetserver.ringutils
-  (:import (clojure.lang ExceptionInfo))
+  (:import (clojure.lang ExceptionInfo)
+           (java.security.cert X509Certificate))
   (:require [clojure.tools.logging :as log]
-            [puppetlabs.kitchensink.core :as ks]))
+            [puppetlabs.kitchensink.core :as ks]
+            [puppetlabs.puppetserver.certificate-authority :as ca]
+            [puppetlabs.certificate-authority.core :as ca-utils]
+            [schema.core :as schema]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Schema
+
+(def WhitelistSettings
+  {(schema/optional-key :authorization-required) schema/Bool
+   :client-whitelist [schema/Str]})
+
+(def RingRequest
+  {:uri schema/Str
+   (schema/optional-key :ssl-client-cert) X509Certificate
+   schema/Keyword schema/Any})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Private
+
+(defn get-cert-subject
+  "Pull the common name of the subject off the certificate."
+  [certificate]
+  (-> certificate
+      (ca/get-subject)
+      (ca-utils/x500-name->CN)))
+
+(defn log-access-denied
+  [uri certificate]
+  "Log a message to info stating that the client is not in the
+   access control whitelist."
+  (let [subject (get-cert-subject certificate)]
+    (log/info
+      (str "Client '" subject "' access to" uri "rejected;\n"
+           "client not found in whitelist configuration."))))
+
+(defn client-on-whitelist?
+  "Test if the certificate subject is on the client whitelist."
+  [settings certificate]
+  (let [whitelist (-> settings
+                      :client-whitelist
+                      (set))
+        client    (get-cert-subject certificate)]
+    (contains? whitelist client)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Public
 
 (defn log-failed-request!
   "Logs a failed HTTP request.
@@ -41,3 +88,19 @@
   [request]
   (if-let [type (:content-type request)]
     (not (empty? (re-find #"^application/(.+\+)?json" type)))))
+
+(schema/defn client-allowed-access? :- schema/Bool
+  "Determines if the client in the request is allowed to access the
+   endpoint based on the client whitelist and
+   whether authorization is required."
+  [settings :- WhitelistSettings
+   req :- RingRequest]
+  (if (get settings :authorization-required true)
+    (if-let [client-cert (:ssl-client-cert req)]
+      (if (client-on-whitelist? settings client-cert)
+        true
+        (do (log-access-denied (:uri req) client-cert) false))
+      (do
+        (log/info "Access to certificate_status rejected; no client certificate found")
+        false))
+    true))
