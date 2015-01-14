e3e5c2349c7886fbcb5c1112937ef24b33466b85
Jeff McCune <jeff@puppetlabs.com>
2015-01-14 09:11:04 -0800

    (MAINT) Update version to 1.0.2-SNAPSHOT for development

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
     :password :env/nexus_jenkins_password
     :sign-releases false })
 
-(defproject puppetlabs/puppet-server "1.0.1"
+(defproject puppetlabs/puppet-server "1.0.2-SNAPSHOT"
   :description "Puppet Server"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
