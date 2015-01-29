5e1b7f02ff25ec9df4bf6ded779f815fe93cc9c8
Jenkins CI <ci@puppetlabs.com>
2015-01-29 09:27:37 -0800

    lein-release plugin: bumped version from 1.0.3 to 1.0.4-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
     :password :env/nexus_jenkins_password
     :sign-releases false })
 
-(defproject puppetlabs/puppet-server "1.0.3"
+(defproject puppetlabs/puppet-server "1.0.4-SNAPSHOT"
   :description "Puppet Server"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
