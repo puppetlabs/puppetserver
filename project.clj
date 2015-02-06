a5c17c4631175761937760045362e47a53b42220
Jenkins CI <ci@puppetlabs.com>
2015-02-05 16:49:19 -0800

    Version 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
     :password :env/nexus_jenkins_password
     :sign-releases false })
 
-(defproject puppetlabs/pe-file-sync "0.0.1"
+(defproject puppetlabs/pe-file-sync "0.0.2-SNAPSHOT"
   :description "PE File Synchronization Services"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
