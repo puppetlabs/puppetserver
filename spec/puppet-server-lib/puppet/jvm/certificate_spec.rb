69f77b01c1b41aa6838c0d443d33b25dc833952b
Justin May <justin.may@puppetlabs.com>
2015-03-13 15:06:44 -0700

    (SERVER-383) Add spec test for new OIDs

--- spec/puppet-server-lib/puppet/jvm/certificate_spec.rb
+++ spec/puppet-server-lib/puppet/jvm/certificate_spec.rb
@@ -31,20 +31,59 @@ describe Puppet::Server::Certificate do
       ext['oid'] == 'pp_uuid' }['value'].should == "ED803750-E3C7-44F5-BB08-41A04433FE2E"
 
     exts.find { |ext|
-      ext['oid'] == 'pp_instance_id' }['value'].should == "1234567890"
+      ext['oid'] == 'pp_instance_id' }['value'].should == "thisisanid"
 
     exts.find { |ext|
       ext['oid'] == 'pp_image_name' }['value'].should == "my_ami_image"
 
     exts.find { |ext|
       ext['oid'] == 'pp_preshared_key' }['value'].should == "342thbjkt82094y0uthhor289jnqthpc2290"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_cost_center' }['value'].should == "center"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_product' }['value'].should == "product"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_project' }['value'].should == "project"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_application' }['value'].should == "application"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_service' }['value'].should == "service"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_employee' }['value'].should == "employee"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_created_by' }['value'].should == "created"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_environment' }['value'].should == "environment"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_role' }['value'].should == "role"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_software_version' }['value'].should == "version"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_department' }['value'].should == "deparment"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_cluster' }['value'].should == "cluster"
+
+    exts.find { |ext|
+      ext['oid'] == 'pp_provisioner' }['value'].should == "provisioner"
   end
 
   it 'should return the proper subject name' do
-    agent_certificate.unmunged_name.should == 'myagent'
+    agent_certificate.unmunged_name.should == 'firehose-agent'
   end
 
   it 'should return the expiration date' do
-    agent_certificate.expiration.to_s.should == '2019-06-09 22:57:49 UTC'
+    agent_certificate.expiration.to_s.should == '2020-03-11 21:44:28 UTC'
   end
 end
