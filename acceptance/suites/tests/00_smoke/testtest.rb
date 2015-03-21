c38f724ebe6fa113d065df9d830cf10ba78ca96c
Jeff McCune <jeff@puppetlabs.com>
2015-03-21 14:52:51 -0700

    (SERVER-499) Fix puppet command not found error on windows
    Without this patch the following error is thrown:
    
        Errored Tests Cases:
          Test Case acceptance/suites/tests/00_smoke/testtest.rb reported: #<RuntimeError: PuppetAcceptance::DSL::Helpers.with_puppet_running_on fail
        ed (check backtrace for location) because: Host 'pdswtmpzet4qbhw.delivery.puppetlabs.net' exited with 127 running:
         puppet agent --test --server fphpsej9y5v05te.delivery.puppetlabs.net
        Last 10 lines of output were:
                bash: puppet: command not found

--- acceptance/suites/tests/00_smoke/testtest.rb
+++ acceptance/suites/tests/00_smoke/testtest.rb
@@ -1,5 +1,5 @@
 test_name "Testing Master/Agent connection"
 
 with_puppet_running_on( master, {} ) do
-  on agents, "puppet agent --test --server #{master}"
+  on agents, puppet("agent --test --server #{master}")
 end
