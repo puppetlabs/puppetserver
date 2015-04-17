63db1c3ae1279e056416b7fa6b79b505831bbc11
Justin May <justin.may@puppetlabs.com>
2015-04-17 15:17:52 -0700

    (SERVER-442) Move params_to_ruby

--- src/ruby/puppet-server-lib/puppet/server/master.rb
+++ src/ruby/puppet-server-lib/puppet/server/master.rb
@@ -34,8 +34,6 @@ class Puppet::Server::Master
 
   def handleRequest(request)
     response = {}
-
-    request["params"] = params_to_ruby(request["params"])
     process(request, response)
     # 'process' returns only the status -
     # `response` now contains all of the response data
@@ -67,25 +65,6 @@ class Puppet::Server::Master
 
   def run_mode()
     Puppet.run_mode.name.to_s
-  end
-
-  private
-
-  def params_to_ruby(params)
-    Hash[params.collect do |key, value|
-      # Values for query string and/or form parameters which are specified
-      # with array-like syntax will be parsed by Ring into a Clojure
-      # PersistentVector, which derives from a Java List.  Need to
-      # translate the Java List into a Ruby Array so that the request
-      # handling logic in Ruby can make use of it.
-
-      # For example, a query string of 'arr=one&arr=two" will be translated
-      # at the Clojure Ring layer into an element with a key of "arr" and
-      # value of '["one", "two"]' as a Clojure PersistentVector.  This
-      # PersistentVector needs to be converted into a Ruby Array before
-      # proceeding with the request processing.
-      [key, value.java_kind_of?(Java::JavaUtil::List) ? value.to_a : value]
-    end]
   end
 
 end
