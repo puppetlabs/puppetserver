5c86b54bf28bc03287f738c4936fefff6b6dedc7
Kevin Corcoran <kwcorcoran@gmail.com>
2015-04-17 14:43:32 -0700

    (maint) Move publish tests to service-test NS
    Move the test cases for the publish endpoint into the service-test
    instead of the core-test because they actually use the Trapperkeeper
    service.

--- test/unit/puppetlabs/enterprise/services/file_sync_storage/file_sync_storage_service_test.clj
+++ test/unit/puppetlabs/enterprise/services/file_sync_storage/file_sync_storage_service_test.clj
@@ -1,21 +1,23 @@
 (ns puppetlabs.enterprise.services.file-sync-storage.file-sync-storage-service-test
-  (:import  (org.eclipse.jgit.api.errors TransportException))
+  (:import (org.eclipse.jgit.api.errors TransportException GitAPIException)
+           (org.eclipse.jgit.api Git))
   (:require [clojure.test :refer :all]
             [puppetlabs.enterprise.file-sync-test-utils :as helpers]
             [puppetlabs.enterprise.services.file-sync-storage.file-sync-storage-core :as core]
             [puppetlabs.enterprise.jgit-client :as client]
             [puppetlabs.enterprise.file-sync-common :as common]
+            [puppetlabs.enterprise.jgit-client :as jgit-client]
+            [puppetlabs.trapperkeeper.testutils.logging :refer [with-test-logging]]
             [puppetlabs.http.client.sync :as http-client]
             [me.raynes.fs :as fs]
-            [cheshire.core :as cheshire]
-            [puppetlabs.enterprise.jgit-client :as jgit-client]
+            [cheshire.core :as json]
             [schema.test :as schema-test]))
 
 (use-fixtures :once schema-test/validate-schemas)
 
 (defn parse-response-body
   [response]
-  (cheshire/parse-string (slurp (:body response))))
+  (json/parse-string (slurp (:body response))))
 
 (defn simple-workflow
   [git-base-dir server-repo-subpath ssl?]
@@ -195,3 +197,167 @@
                         expected-rev (client/head-rev-id-from-working-tree
                                        client-orig-repo-dir-2)]
                     (is (= actual-rev expected-rev))))))))))))
+
+(defn get-commit
+  [repo]
+  (try (-> repo
+           Git/open
+           .log
+           .call
+           first)
+       ;; Want all tests to run even if one test has an error
+       (catch GitAPIException e
+         "Could not get git commit for repo " repo "Error: " e)))
+
+(def publish-url (str helpers/server-base-url
+                      helpers/default-api-path-prefix
+                      common/publish-content-sub-path))
+
+(defn make-publish-request
+  [body]
+  (http-client/post publish-url
+             {:body    (json/encode body)
+              :headers {"Content-Type" "application/json"}}))
+
+(deftest publish-content-endpoint-success-test
+  (testing "publish content endpoint makes correct commit"
+    (let [repo "test-commit.git"
+          working-dir (helpers/temp-dir-as-string)
+          base-path (helpers/temp-dir-as-string)
+          server-repo (fs/file base-path repo)]
+
+      (helpers/add-remote! (helpers/init-repo! (fs/file working-dir))
+                           "origin"
+                           (str (helpers/repo-base-url) "/" repo))
+
+      (helpers/with-bootstrapped-file-sync-storage-service-for-http
+        app
+        (helpers/storage-service-config-with-repos
+          base-path
+          {(keyword repo) {:working-dir working-dir}}
+          false)
+        (testing "with no body supplied"
+          (let [response (http-client/post publish-url)
+                body (slurp (:body response))]
+            (testing "get expected response"
+              (is (= (:status response) 200))
+              (is (= repo (first (keys (json/parse-string body))))
+                  (str "Unexpected response body: " body)))
+            (let [commit (get-commit server-repo)]
+              (testing "commit message is correct"
+                (is (= core/default-commit-message
+                       (.getFullMessage commit))))
+              (testing "commit author is correct"
+                (is (= core/default-commit-author-name (.getName (.getAuthorIdent commit))))
+                (is (= core/default-commit-author-email
+                       (.getEmailAddress (.getAuthorIdent commit))))))))
+
+        (testing "with just author supplied"
+          (let [author {:name  "Tester"
+                        :email "test@example.com"}
+                response (make-publish-request {:author author})
+                body (slurp (:body response))]
+            (testing "get expected response"
+              (is (= (:status response) 200))
+              (is (= repo (first (keys (json/parse-string body))))
+                  (str "Unexpected response body: " body)))
+            (let [commit (get-commit server-repo)]
+              (testing "commit message is correct"
+                (is (= core/default-commit-message
+                       (.getFullMessage commit))))
+              (testing "commit author is correct"
+                (is (= (:name author) (.getName (.getAuthorIdent commit))))
+                (is (= (:email author) (.getEmailAddress (.getAuthorIdent commit))))))))
+
+        (testing "with author and message supplied"
+          (let [author {:name  "Tester"
+                        :email "test@example.com"}
+                message "This is a test commit"
+                response (make-publish-request {:author author :message message})
+                body (slurp (:body response))]
+            (testing "get expected response"
+              (is (= (:status response) 200))
+              (is (= repo (first (keys (json/parse-string body))))
+                  (str "Unexpected response body: " body)))
+            (let [commit (get-commit server-repo)]
+              (testing "commit message is correct"
+                (is (= message (.getFullMessage commit))))
+              (testing "commit author is correct"
+                (is (= (:name author) (.getName (.getAuthorIdent commit))))
+                (is (= (:email author) (.getEmailAddress (.getAuthorIdent commit))))))))))))
+
+(deftest publish-content-endpoint-error-test
+  (testing "publish content endpoint returns well-formed errors"
+    (helpers/with-bootstrapped-file-sync-storage-service-for-http
+      app
+      (helpers/storage-service-config-with-repos
+        (helpers/temp-dir-as-string)
+        {:repo-name {:working-dir (helpers/temp-dir-as-string)}}
+        false)
+
+      (testing "when request body does not match schema"
+        (let [response (make-publish-request {:author "bad"})
+              body (slurp (:body response))]
+          (is (= (:status response) 400))
+          (is (= "user-data-invalid" (get-in (json/parse-string body) ["error" "type"]))
+              (str "Unexpected response body: " body))))
+
+      (testing "when request body is malformed json"
+        (let [response (http-client/post publish-url
+                                  {:body    "malformed"
+                                   :headers {"Content-Type" "application/json"}})
+              body (slurp (:body response))]
+          (is (= (:status response) 400))
+          (is (= "json-parse-error" (get-in (json/parse-string body) ["error" "type"]))
+              (str "Unexpected response body: " body))))
+
+      (testing "when request body is not json"
+        (let [response (http-client/post publish-url {:body    "not json"
+                                               :headers {"Content-Type" "text/plain"}})
+              body (slurp (:body response))]
+          (is (= (:status response) 400))
+          (is (= "content-type-error" (get-in (json/parse-string body) ["error" "type"]))
+              (str "Unexpected response body: " body)))))))
+
+(deftest publish-content-endpoint-response-test
+  (testing "publish content endpoint returns correct response"
+    (let [failed-repo "publish-failed.git"
+          nonexistent-repo "publish-non-existent.git"
+          success-repo "publish-success.git"
+          working-dir-failed (helpers/temp-dir-as-string)
+          working-dir-success (helpers/temp-dir-as-string)
+          base-path (helpers/temp-dir-as-string)]
+
+      (helpers/init-repo! (fs/file working-dir-failed))
+      (helpers/add-remote! (helpers/init-repo! (fs/file working-dir-success))
+                           "origin"
+                           (str (helpers/repo-base-url) "/" success-repo))
+
+      (helpers/with-bootstrapped-file-sync-storage-service-for-http
+        app
+        (helpers/storage-service-config-with-repos
+          base-path
+          {(keyword failed-repo)      {:working-dir working-dir-failed}
+           (keyword nonexistent-repo) {:working-dir "not/a/directory"}
+           (keyword success-repo)     {:working-dir working-dir-success}}
+          false)
+        (with-test-logging
+          (let [response (http-client/post publish-url)
+                body (slurp (:body response))]
+            (testing "get a 200 response"
+              (is (= (:status response) 200)))
+
+            (let [data (json/parse-string body)]
+              (testing "for repo that was successfully published"
+                (is (not= nil (get data success-repo)))
+                (is (= (client/head-rev-id-from-working-tree (fs/file working-dir-success))
+                       (get data success-repo))
+                    (str "Could not find correct body for " failed-repo " in " body)))
+              (testing "for nonexistent repo"
+                (is (re-matches #".*repo-not-found-error"
+                                (get-in data [nonexistent-repo "error" "type"]))
+                    (str "Could not find correct body for " nonexistent-repo " in " body)))
+              (testing "for repo that failed to publish"
+                (is (re-matches #".*publish-error"
+                                (get-in data [failed-repo "error" "type"]))
+                    (str "Could not find correct body for " failed-repo " in " body))))))))))
