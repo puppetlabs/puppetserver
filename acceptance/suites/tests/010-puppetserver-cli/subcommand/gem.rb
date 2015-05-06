daa9af2f5cfbc82d4349cdd285fc45d1eed5e54c
Joe Pinsonault <joe.pinsonault@gmail.com>
2015-05-06 16:02:37 -0700

    (MAINT) Removed cli if statement

--- acceptance/suites/tests/010-puppetserver-cli/subcommand/gem.rb
+++ acceptance/suites/tests/010-puppetserver-cli/subcommand/gem.rb
@@ -3,17 +3,7 @@ require 'puppetserver/acceptance/gem_utils'
 
 test_name "Puppetserver 'gem' subcommand tests."
 
-# --------------------------------------------------------------------------
-# This behavior needs to be baked into Beaker but is not yet. SERVER-79 tracks
-# the need to fix this once Beaker has been modified to make the paths to these
-# commands available.
-#
-if master.is_pe?
-  cli = "pe-puppetserver"
-else
-  cli = "puppetserver"
-end
-# --------------------------------------------------------------------------
+cli = "puppetserver"
 
 # define gems to test
 gems = ['nokogiri', 'excon']
