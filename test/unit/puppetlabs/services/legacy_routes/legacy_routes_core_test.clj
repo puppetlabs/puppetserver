30431b9cce2926b2ede855e88dc58b1cf12c09be
Jeff McCune <jeff@puppetlabs.com>
2015-05-12 11:22:29 -0700

    (SERVER-548) Fail the unit tests if header assertions are skipped
    Without this patch there is the possibility that the assertions made
    against the munged request are not executed.  This is a problem because
    there could be an issue with the routes which are not caught by the unit
    tests.  This patch addresses the problem by creating an Atom and
    wrapping the assertions made from a downstream middleware in a function
    that updates the Atom when triggered.  The patch makes a subsequent
    assertion against the Atom to make sure it was updated by the
    assertion function.

--- test/unit/puppetlabs/services/legacy_routes/legacy_routes_core_test.clj
+++ test/unit/puppetlabs/services/legacy_routes/legacy_routes_core_test.clj
@@ -41,10 +41,10 @@
 
   {path {method {example expected}}}"
   {
-   "/" {:head {"raw" "raw"}
-        :post {"raw" "raw"}
-        :get {"raw" "raw"
-              "foo,bar" "foo,bar"}}
+   "/production/node/anode" { :get {"raw" "binary"
+                                    "foo,bar" "foo, bar"}}
+   "/production/facts/anode" { :get {"raw" "binary"
+                                     "foo,bar" "foo, bar"}}
    "/production/catalog/anode" {:get accept-header-raw-examples
                                 :post accept-header-raw-examples}
    "/production/file_content/something" {:get accept-header-raw-examples}
@@ -75,7 +75,11 @@
   "Validate a request by threading it through the legacy-routes request handler
   into an assertion-wrapper ring middleware."
   [request assertion]
-  ((validating-app assertion) request))
+  (let [mem (atom {:tested false})
+        wrapped-assertion #((swap! mem assoc :tested true) (assertion %))
+        response ((validating-app wrapped-assertion) request)]
+    (is (true? (:tested @mem)) (str "request not tested: " (pr-str request)))
+    response))
 
 (deftest test-legacy-routes
   (let [app     (build-ring-handler
@@ -140,10 +144,6 @@
     (testing (str "Content-Type: application/octet-stream "
                "is added to file_bucket_file put requests")
       (let [request (mock/request :put "/production/file_bucket_file/foo")]
-        (test-munged-request request
-          (fn [munged-request] ; ring middleware
-            (let [actual (get-in munged-request content-type-header)]
-              (is (= "application/octet-stream" actual)))))
         (test-munged-request request
           (fn [munged-request] ; ring middleware
             (let [actual (get-in munged-request content-type-header)]
