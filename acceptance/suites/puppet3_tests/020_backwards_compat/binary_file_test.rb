bba057197a95e12332bb444cc62f31f6ac465b82
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2015-05-17 23:53:24 -0700

    (SERVER-652) Changes to acceptance tests to support Windows
    This commit includes some changes to the Puppet 3.x acceptance tests to
    support testing in a Windows environment.  These include:
    
    1) Dynamically determine the temp directory to store synced simmons
       module content into.
    
    2) Replace use of openssl with md5sum for performing md5 sums of files
       on the agent node.  md5sum is available on Windows nodes via cygwin.
    
    3) Unskip external_fact_output_test for Windows since it is viable for
       Windows agents.

--- acceptance/suites/puppet3_tests/020_backwards_compat/binary_file_test.rb
+++ acceptance/suites/puppet3_tests/020_backwards_compat/binary_file_test.rb
@@ -2,8 +2,8 @@ require 'puppetserver/acceptance/compat_utils'
 
 test_name 'binary file resource'
 
-studio = "/tmp/simmons-studio-#{Process.pid}"
 agent = nonmaster_agents().first
+studio = agent.tmpdir('binary_file_test')
 
 teardown do
   cleanup(studio)
@@ -14,12 +14,12 @@ step "Apply simmons::binary_file to agent(s)" do
 end
 
 step "Validate binary-file" do
-  md5 = on(agent, "openssl dgst -md5 #{studio}/binary-file | awk '{print $2}'").stdout.chomp
+  md5 = on(agent, "md5sum #{studio}/binary-file | awk '{print $1}'").stdout.chomp
   assert_equal('7cfc2db80222ef224d99648716cea8e4', md5)
 end
 
 step "Validate binary-file filebucket backup" do
-  old_md5 = on(agent, "openssl dgst -md5 #{studio}/binary-file-old | awk '{print $2}'").stdout.chomp
+  old_md5 = on(agent, "md5sum #{studio}/binary-file-old | awk '{print $1}'").stdout.chomp
   on(agent, puppet("filebucket restore #{studio}/binary-file-backup #{old_md5} --server #{master}"))
   diff = on(agent, "diff #{studio}/binary-file-old #{studio}/binary-file-backup").exit_code
   assert_equal(0, diff, 'binary-file was not backed up to filebucket')
--- acceptance/suites/puppet3_tests/020_backwards_compat/mount_point_binary_file_test.rb
+++ acceptance/suites/puppet3_tests/020_backwards_compat/mount_point_binary_file_test.rb
@@ -2,8 +2,8 @@ require 'puppetserver/acceptance/compat_utils'
 
 test_name 'binary file resource from custom mount point'
 
-studio = "/tmp/simmons-studio-#{Process.pid}"
 agent = nonmaster_agents().first
+studio = agent.tmpdir('mount_point_binary_file_test')
 
 teardown do
   cleanup(studio)
@@ -14,6 +14,6 @@ step "Apply simmons::mount_point_binary_file to agent(s)" do
 end
 
 step "Validate mount-point-binary-file" do
-  md5 = on(agent, "openssl dgst -md5 #{studio}/mount-point-binary-file | awk '{print $2}'").stdout.chomp
+  md5 = on(agent, "md5sum #{studio}/mount-point-binary-file | awk '{print $1}'").stdout.chomp
   assert_equal('4b392568e0c19886bf274663a63b7d18', md5)
 end
