6dc1cb8c729b5ca075fc49cb4385e48ea1c7f6ee
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2015-05-26 14:45:09 -0700

    (MAINT) Add install options to 'install_dev_repos'
    This commit configures options explicitly for the
    '30_install_dev_repos.rb' for package installs.  This was done
    specifically to configure the use of the 'PC1' repo since this is
    required for use with AIO testing.  The need for this change was
    introduced by the beaker refactoring done for BKR-184 which removed PC1
    from the default list of repos that beaker checks during installation.

--- acceptance/suites/pre_suite/foss/30_install_dev_repos.rb
+++ acceptance/suites/pre_suite/foss/30_install_dev_repos.rb
@@ -1,9 +1,15 @@
+install_opts = {
+  :dev_builds_repos => ["PC1"],
+  :dev_builds_url => 'http://builds.delivery.puppetlabs.net'}
+repo_config_dir = 'tmp/repo_configs'
+
 case test_config[:puppetserver_install_type]
 when :package
   step "Setup Puppet Server repositories." do
     package_build_version = ENV['PACKAGE_BUILD_VERSION']
     if package_build_version
-      install_puppetlabs_dev_repo master, 'puppetserver', package_build_version
+      install_puppetlabs_dev_repo master, 'puppetserver', package_build_version,
+                                  repo_config_dir, install_opts
     else
       abort("Environment variable PACKAGE_BUILD_VERSION required for package installs!")
     end
@@ -15,7 +21,8 @@ if puppet_build_version
   confine_block :except, :platform => ['windows'] do
     step "Setup Puppet Labs Dev Repositories." do
       hosts.each do |host|
-        install_puppetlabs_dev_repo host, 'puppet-agent', puppet_build_version
+        install_puppetlabs_dev_repo host, 'puppet-agent', puppet_build_version,
+                                    repo_config_dir, install_opts
       end
     end
   end
