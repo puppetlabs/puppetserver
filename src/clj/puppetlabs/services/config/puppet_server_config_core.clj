7c047313f059ba30266552380f833db40e7b1915
Kevin Corcoran <kwcorcoran@gmail.com>
2015-05-27 17:29:12 -0700

    (maint) Add codedir to list of Puppet settings

--- src/clj/puppetlabs/services/config/puppet_server_config_core.clj
+++ src/clj/puppetlabs/services/config/puppet_server_config_core.clj
@@ -27,6 +27,7 @@
     :certdir
     :certname
     :cert-inventory
+    :codedir ; This is not actually needed in Puppet Server, but it's needed in PE (file sync)
     :csrdir
     :csr-attributes
     :dns-alt-names
