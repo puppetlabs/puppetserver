8cb9ea49a16f78f70299bca4aca27407e840c479
Kevin Corcoran <kwcorcoran@gmail.com>
2015-06-11 13:03:41 -0700

    (PE-7741) Add trapperkeeper-status dependency
    ... and update all the dependencies to make :pedantic? :abort happy.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def tk-version "1.1.1")
 (def tk-jetty-version "1.3.1")
-(def ks-version "1.0.0")
+(def ks-version "1.1.0")
 (def jgit-version "3.7.0.201502260915-r")
 
 (defn deploy-info
@@ -14,15 +14,16 @@
   :description "PE File Synchronization Services"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [puppetlabs/trapperkeeper ~tk-version]
-                 [puppetlabs/kitchensink ~ks-version]
+                 [puppetlabs/trapperkeeper ~tk-version :exclusions [clj-time org.clojure/tools.macro]]
+                 [puppetlabs/kitchensink ~ks-version :exclusions [clj-time slingshot]]
                  [puppetlabs/http-client "0.4.4" :exclusions [commons-io]]
-                 [puppetlabs/ssl-utils "0.8.0"]
+                 [puppetlabs/ssl-utils "0.8.0" :exclusions [clj-time]]
                  [puppetlabs/trapperkeeper-scheduler "0.0.1"]
+                 [puppetlabs/trapperkeeper-status "0.1.2"]
                  [prismatic/schema "0.4.0"]
-                 [compojure "1.1.8"]
+                 [compojure "1.3.3"]
                  [overtone/at-at "1.2.0"]
-                 [ring/ring-json "0.3.1"]
+                 [ring/ring-json "0.3.1" :exclusions [ring/ring-core]]
                  [org.eclipse.jgit/org.eclipse.jgit.http.server ~jgit-version
                   :exclusions [org.apache.httpcomponents/httpclient]]
                  [org.eclipse.jgit/org.eclipse.jgit.http.apache ~jgit-version
@@ -42,10 +43,10 @@
 
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                                  [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version]
-                                  [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version :classifier "test"]
-                                  [puppetlabs/trapperkeeper ~tk-version :classifier "test" :scope "test"]
-                                  [puppetlabs/kitchensink ~ks-version :classifier "test" :scope "test"]
+                                  [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version :exclusions [clj-time]]
+                                  [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version :classifier "test" :exclusions [clj-time]]
+                                  [puppetlabs/trapperkeeper ~tk-version :classifier "test" :scope "test" :exclusions [clj-time org.clojure/tools.macro]]
+                                  [puppetlabs/kitchensink ~ks-version :classifier "test" :scope "test" :exclusions [clj-time slingshot]]
                                   [spyscope "0.1.4" :exclusions [clj-time]]]
                    :injections [(require 'spyscope.core)]}}
 
