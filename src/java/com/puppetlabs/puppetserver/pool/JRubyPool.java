c6e8b26e0ba1805c93a8ff7c25fc24a036026a92
Ruth Linehan <ruth@puppetlabs.com>
2015-09-23 15:57:12 +0100

    (SERVER-860) Add javadocs to LockablePool interface and JRubyPool class

--- src/java/com/puppetlabs/puppetserver/pool/JRubyPool.java
+++ src/java/com/puppetlabs/puppetserver/pool/JRubyPool.java
@@ -7,6 +7,16 @@ import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import com.puppetlabs.puppetserver.pool.LockablePool;
 
+/**
+ * A data structure to be used as a Pool, encapsulating a LinkedBlockingDeque
+ * and a ReentrantReadWriteLock. Implements the <tt>LockablePool</tt>
+ * interface such that when a <tt>borrowItem</tt> is performed an instance is
+ * taken from the deque and a read lock is acquired, when a
+ * <tt>returnItem</tt> is performed an instance is put back onto the deque and
+ * a read lock is released, and <tt>lock</tt> locks a write lock.
+ *
+ * @param <E> the type of element that can be added to the LinkedBlockingDeque.
+ */
 public final class JRubyPool<E> implements LockablePool<E> {
     private final LinkedBlockingDeque<E> liveQueue;
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
@@ -16,12 +26,39 @@ public final class JRubyPool<E> implements LockablePool<E> {
         liveQueue = new LinkedBlockingDeque<>(size);
     }
 
+    /**
+    * This method is analagous to <tt>putLast</tt> in the
+    * <tt>LinkedBlockingDeque</tt> class, but also causes the element to be
+    * added to the list of "registered" elements that will be returned by
+    * <tt>getRegisteredInstances</tt>.
+    *
+    * Note that this method is synchronized to try to ensure that the addition
+    * to the queue and the list of registered instances are visible roughly
+    * atomically to consumers, but because the underlying queue uses its own
+    * lock, it is possible for it to be modified on another thread while this
+    * method is being executed.
+    *
+    * @param e the element to register and put at the end of the queue.
+    *
+    * @throws InterruptedException
+    */
     @Override
     synchronized public void register(E e) throws InterruptedException {
         liveQueue.putLast(e);
         registeredElements.add(e);
     }
 
+    /**
+    * This method is analagous to <tt>takeFirst</tt> in the
+    * <tt>LinkedBlockingDeque</tt> class, but also causes a read lock to be
+    * locked from the <tt>ReentrantReadWriteLock</tt>.
+    *
+    * @return The head of the deque.
+    *
+    * @throws IllegalStateException if the thread holding the write lock
+    *         attempts to call this method.
+    * @throws InterruptedException
+    */
     @Override
     public E borrowItem() throws InterruptedException, IllegalStateException {
         if (lock.isWriteLockedByCurrentThread()) {
@@ -32,6 +69,22 @@ public final class JRubyPool<E> implements LockablePool<E> {
         return item;
     }
 
+    /**
+    * This method is analagous to <tt>pollFirst</tt> in the
+    * <tt>LinkedBlockingDeque</tt> class, but also causes a read lock to be
+    * locked from the <tt>ReentrantReadWriteLock</tt>.
+    *
+    * @param timeout how long to wait before giving up, in units of unit
+    * @param unit a <tt>TimeUnit</tt> determining how to interpret the
+    *        <tt>timeout parameter</tt>
+    *
+    * @return The head of the deque, or <tt>null</tt> if the specified waiting
+    *         time elapses before an element is available.
+    *
+    * @throws IllegalStateException if the thread holding the write lock
+    *         attempts to call this method.
+    * @throws InterruptedException
+    */
     @Override
     public E borrowItemWithTimeout(long timeout, TimeUnit unit) throws InterruptedException, IllegalStateException {
         if (lock.isWriteLockedByCurrentThread()) {
@@ -42,6 +95,15 @@ public final class JRubyPool<E> implements LockablePool<E> {
         return item;
     }
 
+    /**
+    * This method is analagous to <tt>putFirst</tt> in the
+    * <tt>LinkedBlockingDeque</tt> class, but also causes a read lock to be
+    * unlocked from the <tt>ReentrantReadWriteLock</tt>.
+    *
+    * @param e the element to add to the queue
+    *
+    * @throws InterruptedException
+    */
     @Override
     public void returnItem(E e) throws InterruptedException {
         try {
@@ -52,31 +114,70 @@ public final class JRubyPool<E> implements LockablePool<E> {
         }
     }
 
+    /**
+    * This method is analagous to <tt>putFirst</tt> in the
+    * <tt>LinkedBlockingDeque</tt> class. It should only ever be used to
+    * insert a `PoisonPill` or `RetryPoisonPill` to the pool, which is an
+    * operation that should be done without acquiring a read lock.
+    *
+    * @param e the element to add to the queue
+    *
+    * @throws InterruptedException
+    */
     @Override
     public void insertPill(E e) throws InterruptedException {
         liveQueue.putFirst(e);
     }
 
+    /**
+    * This method is analagous to <tt>clear</tt> in the
+    * <tt>LinkedBlockingDeque</tt> class.
+    *
+    * @throws InterruptedException
+    */
     @Override
     public void clear() {
         liveQueue.clear();
     }
 
+    /**
+    * This method is analagous to <tt>remainingCapacity</tt> in the
+    * <tt>LinkedBlockingDeque</tt> class.
+    *
+    * @return the number of additional elements that the queue can ideally
+    *         accept without blocking.
+    */
     @Override
     public int remainingCapacity() {
         return liveQueue.remainingCapacity();
     }
 
+    /**
+    * This method is analagous to <tt>size</tt> in the
+    * <tt>LinkedBlockingDeque</tt> class.
+    *
+    * @return the number of elements in the queue
+    */
     @Override
     public int size() {
         return liveQueue.size();
     }
 
+    /**
+    * Acquires a write lock from <tt>ReentrantReadWriteLock</tt>.
+    *
+    * Note that this implementation does not fulfill requirement (e); with
+    * this implementation, the thread holding the lock cannot perform a borrow
+    * while it's holding the lock.
+    */
     @Override
     public void lock() throws InterruptedException {
         lock.writeLock().lock();
     }
 
+    /**
+    * Releases the write lock.
+    */
     @Override
     public void unlock() throws InterruptedException {
         lock.writeLock().unlock();
@@ -84,7 +185,7 @@ public final class JRubyPool<E> implements LockablePool<E> {
 
     /**
      * @return a set of all of the known elements that have been registered with
-     *         this queue.
+     *         this pool.
      */
     public Set<E> getRegisteredElements() {
         return registeredElements;
--- src/java/com/puppetlabs/puppetserver/pool/LockablePool.java
+++ src/java/com/puppetlabs/puppetserver/pool/LockablePool.java
@@ -4,23 +4,79 @@ import java.util.concurrent.TimeUnit;
 
 public interface LockablePool<E> {
 
+   /**
+    * Introduce a new instance to the pool in a way that allows us to keep track
+    * of the list of all existing instances, even if some of them are
+    * borrowed.
+    */
     void register(E e) throws InterruptedException;
 
+   /**
+    * Borrow an instance. This and <tt>borrowItemWithTimeout</tt> are the
+    * only entry points for accessing instances.
+    */
     E borrowItem() throws InterruptedException;
 
+   /**
+    * Borrow an instance, waiting up to the specified timeout if necessary
+    * for an instance to become available. Returns `null` if the wait time
+    * elapses before an instance is available.
+    *
+    * This and <tt>borrowItemWithTimeout</tt> are the only entry points for
+    * accessing instances.
+    */
     E borrowItemWithTimeout(long timout, TimeUnit unit) throws InterruptedException;
 
+   /**
+    * Return an instance to the pool.
+    */
     void returnItem(E e) throws InterruptedException;
 
+   /**
+    * Insert a poison pill to the pool. This is different from returning an
+    * instance to the pool because there are different locking semantics
+    * around inserting a pill.
+    */
     void insertPill(E e) throws InterruptedException;
 
+    /**
+     * Clear the pool.
+     */
     void clear();
 
+    /**
+     * Returns the number of additional items that the pool can accept without
+     * blocking. Equal to the initial capacity of the pool minus the current
+     * <tt>size</tt> of the pool.
+     */
     int remainingCapacity();
 
+    /**
+     * Returns the number of elements in the pool.
+     */
     int size();
 
+   /**
+    * Lock the pool. This method should make the following guarantees:
+    *
+    *  a) blocks until all registered instances are returned to the pool
+    *  b) once this method is called (even before it returns), any new threads
+    *     that attempt a <tt>borrow</tt> should block until <tt>unlock()</tt>
+    *     is called
+    *  c) if there are other threads that were already blocking in a
+    *     <tt>borrow</tt> before this method was called, they should continue
+    *     to block until <tt>unlock()</tt> is called
+    *  d) instances may be returned by other threads while this method is
+    *     being executied
+    *  e) when the method returns, the caller holds an exclusive lock on the
+    *     pool; the lock should be re-entrant in the sense that this thread
+    *     should still be able to perform borrows while it's holding the lock
+    */
     void lock() throws InterruptedException;
 
+   /**
+    * Release the exclusive lock so that other threads may begin to perform
+    * borrow operations again.
+    */
     void unlock() throws InterruptedException;
 }
