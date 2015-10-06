5d99e36ec22215ed6f6bebf79f421858048a4ac2
erikPrime <erik@puppetlabs.com>
2015-10-06 08:36:34 -0700

    TK-Auth all methods testing against default rules initial commit

--- acceptance/scripts/generic/testrun.sh
+++ acceptance/scripts/generic/testrun.sh
@@ -5,10 +5,8 @@ set -x
 while [ ! -d acceptance ] ; do cd ..; done
 
 export GEM_SOURCE=http://rubygems.delivery.puppetlabs.net
-
-#The reason for these exports is that someday we want to pick them up in a rake file
 export GENCONFIG_LAYOUT="${GENCONFIG_LAYOUT:-redhat6-64ma-ubuntu1404-64a-windows2008r2-64a}"
-export BEAKER_TESTSUITE="${BEAKER_TESTSUITE:-acceptance/suites/tests/00_smoke}"
+export BEAKER_TESTSUITE="${BEAKER_TESTSUITE:-acceptance/suites/tests/}"
 export BEAKER_PRESUITE="${BEAKER_PRESUITE:-acceptance/suites/pre_suite/foss}"
 export BEAKER_POSTSUITE="acceptance/suites/post_suite/"
 export BEAKER_OPTIONS="acceptance/config/beaker/options.rb"
@@ -22,7 +20,6 @@ bundle install --path vendor/bundle
 BEAKER="bundle exec beaker --debug"
 BEAKER="$BEAKER --type aio"
 BEAKER="$BEAKER --keyfile $BEAKER_KEYFILE"
-BEAKER="$BEAKER --tests $BEAKER_TESTSUITE"
 BEAKER="$BEAKER --helper $BEAKER_HELPER"
 BEAKER="$BEAKER --options-file $BEAKER_OPTIONS"
 BEAKER="$BEAKER --post-suite $BEAKER_POSTSUITE"
@@ -33,19 +30,16 @@ case $1 in
   -p | --p* )
   bundle exec genconfig2 $GENCONFIG_LAYOUT > $BEAKER_CONFIG
     
-  if [ -z "$BUILD_PACKAGE_VERSION" ]; 
+  if [ -z "$PACKAGE_BUILD_VERSION" ]; 
     #TODO: curl builds.puppetlabs.lan/puppetserver.  Parse HTML, find most recent folder name.
-    then echo "$0: BUILD_PACKAGE_VERSION not set.  Please export BUILD_PACAKGE_VERSION."
-    exit -1;
-  fi
-
-  if [ -z BEAKER_PRESUITE ]; then echo "$0: BEAKER_PRESUITE not set."
+    then echo "$0: PACKAGE_BUILD_VERSION not set.  Please export PACKAGE_BUILD_VERSION."
     exit -1;
   fi
   
   BEAKER="$BEAKER --pre-suite $BEAKER_PRESUITE"
   BEAKER="$BEAKER --config $BEAKER_CONFIG"
   BEAKER="$BEAKER --preserve-hosts"
+  BEAKER="$BEAKER --tests $BEAKER_TESTSUITE"
   $BEAKER
   echo "Beaker exited with $?"
   cp log/latest/hosts_preserved.yml .
@@ -66,6 +60,7 @@ case $1 in
   
   BEAKER="$BEAKER --config hosts_preserved.yml"
   BEAKER="$BEAKER --preserve-hosts"
+  BEAKER="$BEAKER --tests $BEAKER_TESTSUITE"
   $BEAKER
   ;;
 
