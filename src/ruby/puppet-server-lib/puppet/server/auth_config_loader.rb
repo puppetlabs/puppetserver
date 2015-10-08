4fe679b4017ea782429a56bb24773fbf84f56e57
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2015-10-07 17:30:56 -0700

    (SERVER-972) Register dummy AuthConfigLoader when not using legacy auth
    This commit registers a dummy AuthConfigLoader with core Ruby Puppet
    when Puppet Server is configured with a `use-legacy-auth-conf` of
    `false`.  This is done to avoid having the auth.conf be parsed (and
    corresponding messages logged) by core Ruby Puppet.

---
+++ src/ruby/puppet-server-lib/puppet/server/auth_config_loader.rb
@@ -0,0 +1,13 @@
+## This is a "dummy" AuthConfigLoader that Puppet Server registers with core
+## Puppet for cases where Puppet Server decides that it should be in charge of
+## authorizing requests at the Clojure / Ring handler level - depending upon
+## the configuration of the 'use_legacy_auth_conf' setting - and not core
+## Puppet.
+
+require 'puppet/server/auth_config'
+
+class Puppet::Server::AuthConfigLoader
+  def self.authconfig
+    Puppet::Server::AuthConfig.new
+  end
+end
