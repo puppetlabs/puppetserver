a120bb12966eef1a843e96a57b199a365efc3cb0
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2015-11-10 17:12:11 -0800

    (MAINT) Bump puppet submodule to 28a10df
    This commit bumps the Puppet submodule up to version 28a10df and
    corresponding puppet-agent dependency up to d5a07cfe7.

--- acceptance/lib/helper.rb
+++ acceptance/lib/helper.rb
@@ -30,7 +30,7 @@ module PuppetServerExtensions
     puppet_build_version = get_option_value(options[:puppet_build_version],
                          nil, "Puppet Development Build Version",
                          "PUPPET_BUILD_VERSION",
-                         "ea26968d5d57fedda32b62b449ea83924f31ff77", :string)
+                         "d5a07cfe7746c3bb9236388cdc5f7a88aa473731", :string)
 
     @config = {
       :base_dir => base_dir,
