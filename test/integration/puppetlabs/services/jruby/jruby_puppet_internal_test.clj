3583c9bd839eecbb6c09a8072504db2e86ee6d01
Chris Price <chris@puppetlabs.com>
2016-01-02 20:52:35 -0800

    (MAINT) Whitespace changes

--- test/integration/puppetlabs/services/jruby/jruby_puppet_internal_test.clj
+++ test/integration/puppetlabs/services/jruby/jruby_puppet_internal_test.clj
@@ -26,35 +26,35 @@
     (is (thrown? ExceptionInfo
                  (jruby-internal/get-compile-mode :foo)))))
 
-  (deftest ^:integration settings-plumbed-into-jruby-container
-    (testing "setting plumbed into jruby container for"
-      (let [pool (JRubyPool. 1)
-            config (jruby-testutils/jruby-puppet-config
-                     {:http-client-connect-timeout-milliseconds 2
-                      :http-client-idle-timeout-milliseconds 5
-                      :http-client-cipher-suites ["TLS_RSA_WITH_AES_256_CBC_SHA256"
-                                                  "TLS_RSA_WITH_AES_256_CBC_SHA"]
-                      :http-client-ssl-protocols ["TLSv1" "TLSv1.2"]
-                      :compile-mode :jit})
-            instance (jruby-internal/create-pool-instance! pool 0 config #() nil)
-            container (:scripting-container instance)]
-        (= RubyInstanceConfig$CompileMode/JIT
-           (.getCompileMode container))
-        (try
-          (let [settings (into {} (.runScriptlet container
-                                    "java.util.HashMap.new
-                                       (Puppet::Server::HttpClient.settings)"))]
-            (testing "http_connect_timeout_milliseconds"
-              (is (= 2 (settings "http_connect_timeout_milliseconds"))))
-            (testing "http_idle_timeout_milliseconds"
-              (is (= 5 (settings "http_idle_timeout_milliseconds"))))
-            (testing "cipher_suites"
-              (is (= ["TLS_RSA_WITH_AES_256_CBC_SHA256"
-                      "TLS_RSA_WITH_AES_256_CBC_SHA"]
-                    (into [] (settings "cipher_suites")))))
-            (testing "ssl_protocols"
-              (is (= ["TLSv1" "TLSv1.2"]
-                    (into [] (settings "ssl_protocols"))))))
-          (finally
-            (.terminate (:jruby-puppet instance))
-            (.terminate container))))))
+(deftest ^:integration settings-plumbed-into-jruby-container
+  (testing "setting plumbed into jruby container for"
+    (let [pool (JRubyPool. 1)
+          config (jruby-testutils/jruby-puppet-config
+                  {:http-client-connect-timeout-milliseconds 2
+                   :http-client-idle-timeout-milliseconds 5
+                   :http-client-cipher-suites ["TLS_RSA_WITH_AES_256_CBC_SHA256"
+                                               "TLS_RSA_WITH_AES_256_CBC_SHA"]
+                   :http-client-ssl-protocols ["TLSv1" "TLSv1.2"]
+                   :compile-mode :jit})
+          instance (jruby-internal/create-pool-instance! pool 0 config #() nil)
+          container (:scripting-container instance)]
+      (= RubyInstanceConfig$CompileMode/JIT
+         (.getCompileMode container))
+      (try
+        (let [settings (into {} (.runScriptlet container
+                                               "java.util.HashMap.new
+                                                  (Puppet::Server::HttpClient.settings)"))]
+          (testing "http_connect_timeout_milliseconds"
+            (is (= 2 (settings "http_connect_timeout_milliseconds"))))
+          (testing "http_idle_timeout_milliseconds"
+            (is (= 5 (settings "http_idle_timeout_milliseconds"))))
+          (testing "cipher_suites"
+            (is (= ["TLS_RSA_WITH_AES_256_CBC_SHA256"
+                    "TLS_RSA_WITH_AES_256_CBC_SHA"]
+                   (into [] (settings "cipher_suites")))))
+          (testing "ssl_protocols"
+            (is (= ["TLSv1" "TLSv1.2"]
+                   (into [] (settings "ssl_protocols"))))))
+        (finally
+          (.terminate (:jruby-puppet instance))
+          (.terminate container))))))
