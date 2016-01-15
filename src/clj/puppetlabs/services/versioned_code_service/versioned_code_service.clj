e86c49e57cf503d8ca501ae1e3f18bdba4c1a57b
Matthaus Owens <matthaus@puppetlabs.com>
2016-01-14 16:21:43 -0800

    (SERVER-1079) Add comment explaining the cases for current-code-id

--- src/clj/puppetlabs/services/versioned_code_service/versioned_code_service.clj
+++ src/clj/puppetlabs/services/versioned_code_service/versioned_code_service.clj
@@ -18,6 +18,13 @@
          {:keys [exit-code stderr stdout]} (shell-utils/execute-command code-id-script [environment])]
      ; TODO Decide what to do about normalizing/sanitizing output with respect to
      ; control characters and encodings
+
+     ;; There are three cases we care about here:
+     ;; - exit code is 0 and no stderr generated: groovy. return stdout
+     ;; - exit code is 0 and stderr was generated: that's fine. log an error
+     ;;   about the stderr and return stdout.
+     ;; - exit code is non-zero: oh no! log an error with all the details and
+     ;;   return nil
      (if (zero? exit-code)
        (do
          (when-not (string/blank? stderr)
