074b31be5d520925c56ba6b1ace220c665124954
Sean P. McDonald <sean.mcdonald@puppetlabs.com>
2016-02-19 10:13:41 -0800

    (maint) Bump EZBake version
    This comit bumps ther version of EZBake
    due to a new release

--- project.clj
+++ project.clj
@@ -110,7 +110,7 @@
              :ezbake {:dependencies ^:replace [[puppetlabs/puppetserver ~ps-version]
                                                [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version]
                                                [org.clojure/tools.nrepl "0.2.3"]]
-                      :plugins [[puppetlabs/lein-ezbake "0.3.21"]]
+                      :plugins [[puppetlabs/lein-ezbake "0.3.23"]]
                       :name "puppetserver"}
              :uberjar {:aot [puppetlabs.trapperkeeper.main]
                        :dependencies [[puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version]]}
