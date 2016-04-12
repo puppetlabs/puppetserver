1b5d0dd651976aa1feff0e32e8c113aec41ff193
Matthaus Owens <matthaus@puppetlabs.com>
2016-04-12 16:16:12 -0700

    (maint) Update comment explaining always_retry_plugins to be more clear

--- src/ruby/puppet-server-lib/puppet/server/puppet_config.rb
+++ src/ruby/puppet-server-lib/puppet/server/puppet_config.rb
@@ -23,7 +23,8 @@ class Puppet::Server::PuppetConfig
 
     # (SERVER-410) Cache features in puppetserver for performance.  Avoiding
     # the cache is intended for agents to reload features mid-catalog-run.
-    # As of (PUP-5482) always_retry_plugins includes always_cache_features.
+    # As of (PUP-5482) setting always_retry_plugins to false implies that
+    # features will always be cached.
     if Puppet.settings.setting('always_retry_plugins')
       Puppet[:always_retry_plugins] = false
     end
