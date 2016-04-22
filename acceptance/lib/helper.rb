0c42e233e61313bbc45eece6ffa45b5294b83c88
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2016-04-22 09:31:16 -0700

    (MAINT) Bump puppet agent dep to da1e61e
    This commit bumps the puppet-agent dependency up to da1e61e and the
    corresponding Ruby puppet submodule up to 57315f.

--- acceptance/lib/helper.rb
+++ acceptance/lib/helper.rb
@@ -30,7 +30,7 @@ module PuppetServerExtensions
     puppet_build_version = get_option_value(options[:puppet_build_version],
                          nil, "Puppet Agent Development Build Version",
                          "PUPPET_BUILD_VERSION",
-                         "8fd6efcae71d49291bbaef236f04bcb58a6feb72", :string)
+                         "da1e61eb7308be2617c3ebcee41785f49a5eac8c", :string)
 
     # puppetdb version corresponds to packaged development version located at:
     # http://builds.delivery.puppetlabs.net/puppetdb/
