0431424f343f98fef770728aae85b39d554cc122
Morgan Rhodes <morgan@puppet.com>
2016-05-12 15:43:26 -0700

    (maint) Update ezbake to pick up systemd for xenial

--- project.clj
+++ project.clj
@@ -126,7 +126,7 @@
                                                [puppetlabs/puppetserver ~ps-version]
                                                [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version]
                                                [org.clojure/tools.nrepl "0.2.3"]]
-                      :plugins [[puppetlabs/lein-ezbake "0.3.25"]]
+                      :plugins [[puppetlabs/lein-ezbake "0.4.0"]]
                       :name "puppetserver"}
              :uberjar {:aot [puppetlabs.trapperkeeper.main]
                        :dependencies [[puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version]]}
