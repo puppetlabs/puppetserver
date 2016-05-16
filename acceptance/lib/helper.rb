d3cbe214f2e65ac2a6e0b6c42dd0aa28697a4b1f
Ruth Linehan <ruth@puppetlabs.com>
2016-05-16 12:18:13 -0700

    (SERVER-1334) Bump puppet and hiera versions
    To what are included in puppet-agent 1.5.0.

--- acceptance/lib/helper.rb
+++ acceptance/lib/helper.rb
@@ -30,7 +30,7 @@ module PuppetServerExtensions
     puppet_build_version = get_option_value(options[:puppet_build_version],
                          nil, "Puppet Agent Development Build Version",
                          "PUPPET_BUILD_VERSION",
-                         "ab9e395ef55565a8e896bd847e90a1c3dfb225f5", :string)
+                         "732e883733fe5e5989afe330e3c5cea00b678d1e", :string)
 
     # puppetdb version corresponds to packaged development version located at:
     # http://builds.delivery.puppetlabs.net/puppetdb/
