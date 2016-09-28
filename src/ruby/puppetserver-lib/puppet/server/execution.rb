737761a677b01b585969c565e6ef51dd1ce5a213
Chris Price <chris@puppetlabs.com>
2016-09-27 17:33:00 -0700

    (SERVER-1570) ExecutionStub should raise error on failonfail
    Prior to this commit, Puppet Server's implementation of `ExecutionStub`
    would ignore the `:failonfail` option.  The documented behavior is to
    raise a `Puppet::ExecutionFailure` if the command returns a non-zero
    exit code.
    
    This commit refactors the ExecutionStub code a bit to allow the
    options hash to be passed in (which also required some refactoring
    of the tests).  Then, if `:failonfail` is set, we raise the appropriate
    error when the exit code is non-zero.

--- src/ruby/puppetserver-lib/puppet/server/execution.rb
+++ src/ruby/puppetserver-lib/puppet/server/execution.rb
@@ -7,31 +7,41 @@ class Puppet::Server::Execution
   def self.initialize_execution_stub
     Puppet::Util::ExecutionStub.set do |command, options, stdin, stdout, stderr|
       if command.is_a?(Array)
+        orig_command_str = command.join(" ")
         binary = command.first
         args = command[1..-1]
       else
+        orig_command_str = command
         binary = command
         args = nil
       end
 
-      # TODO - options is currently ignored - https://tickets.puppetlabs.com/browse/SERVER-74
+      # TODO - options relating to stdout/stderr are not yet handled, see SERVER-74 and
+      #  PUP-6640
 
       # We're going to handle STDIN/STDOUT/STDERR in java, so we don't need
       # them here.  However, Puppet::Util::Execution.execute doesn't close them
       # for us, so we have to do that now.
       [stdin, stdout, stderr].each { |io| io.close rescue nil }
 
-      execute(binary, args)
+      execute(orig_command_str, binary, options, args)
     end
   end
 
-  def self.execute(command, args = nil)
+  private
+  def self.execute(orig_command_str, command, options, args)
     if args && !args.empty?
       result = ShellUtils.executeCommand(command, args.to_java(:string))
     else
       result = ShellUtils.executeCommand(command)
     end
 
-    Puppet::Util::Execution::ProcessOutput.new(result.getOutput, result.getExitCode)
+    # TODO - not all options from Puppet::Util::Execution are supported yet, see SERVER-74
+
+    if options[:failonfail] and result.exit_code != 0
+      raise Puppet::ExecutionFailure, "Execution of '#{orig_command_str}' returned #{result.exit_code}: #{result.output.strip}"
+    end
+
+    Puppet::Util::Execution::ProcessOutput.new(result.output, result.exit_code)
   end
 end
