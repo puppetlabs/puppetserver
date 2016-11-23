8cb23108fce65f3588a5372ee2a1a2573752c170
Ruth Linehan <ruth@puppetlabs.com>
2016-11-23 15:33:47 -0800

    (SERVER-377) Bump to jruby-utils 0.5.0
    Bump to jruby-utils 0.5.0, which is the released version of jruby-utils that
    includes change to allow proxy-related environment variables to pass through
    to the JRuby scripting container.

--- project.clj
+++ project.clj
@@ -58,7 +58,7 @@
                  ;; in different versions of the three different logback artifacts
                  [net.logstash.logback/logstash-logback-encoder]
 
-                 [puppetlabs/jruby-utils "0.4.2-SNAPSHOT"]
+                 [puppetlabs/jruby-utils "0.5.0"]
                  [puppetlabs/trapperkeeper]
                  [puppetlabs/trapperkeeper-authorization]
                  [puppetlabs/trapperkeeper-scheduler]
