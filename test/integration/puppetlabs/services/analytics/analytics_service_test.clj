1b760defe440093306dee697cd4bcd49b44ea1a3
Jeremy Barlow <jeremy.barlow@puppet.com>
2017-03-09 08:51:10 -0800

    (SERVER-1743) Rework check-for-updates tests for timing issues
    Previously, the check-for-updates tests could intermittently fail
    because the check-for-updates call can happen later than when assertions
    for the call are made.  In this commit, the tests have been reworked to
    wait a longer period of time for the call to be made.  To minimize test
    run time, the maximum wait time is much longer for the opt-in case vs.
    the opt-out case.

--- test/integration/puppetlabs/services/analytics/analytics_service_test.clj
+++ test/integration/puppetlabs/services/analytics/analytics_service_test.clj
@@ -7,13 +7,14 @@
 
 (deftest ^:integration version-check-test
   (testing "master calls into the dujour version check library using the correct values"
-    ; This atom will store the parameters passed to the version-check-test-fn, which allows us to keep the
+    ; This promise will store the parameters passed to the version-check-test-fn, which allows us to keep the
     ; assertions about their values inside the version-check-test and will also ensure failures will appear if
     ; the master stops calling the check-for-updates! function
-    (let [version-check-params (atom {})
+    (let [version-check-params (promise)
           version-check-test-fn (fn [request-values update-server-url]
-                                  (swap! version-check-params #(assoc % :request-values request-values
-                                                                        :update-server-url update-server-url)))]
+                                  (deliver version-check-params
+                                           {:request-values request-values
+                                            :update-server-url update-server-url}))]
       (with-redefs
        [version-check/check-for-updates! version-check-test-fn]
         (logutils/with-test-logging
@@ -26,18 +27,26 @@
            :product {:update-server-url "http://notarealurl/"
                      :name {:group-id "puppets"
                             :artifact-id "yoda"}}}
-          (is (= {:group-id "puppets" :artifact-id "yoda"}
-                 (get-in @version-check-params [:request-values :product-name])))
-          (is (= "http://notarealurl/" (:update-server-url @version-check-params))))))))
+          (let [params-received (deref version-check-params 30000
+                                       {:request-values
+                                        {:product-name
+                                         :no-product-name-received-before-time-out-reached}
+                                        :update-server-url
+                                        :no-update-server-url-received-before-time-out-reached})]
+            (is (= {:group-id "puppets" :artifact-id "yoda"}
+                   (get-in params-received [:request-values :product-name])))
+            (is (= "http://notarealurl/"
+                   (:update-server-url params-received)))))))))
 
   (testing "master does not make an analytics call to dujour if opt-out exists"
-    ; This atom will store the parameters passed to the version-check-test-fn, which allows us to keep the
+    ; This promise will store the parameters passed to the version-check-test-fn, which allows us to keep the
     ; assertions about their values inside the version-check-test and will also ensure failures will appear if
     ; the master stops calling the check-for-updates! function
-    (let [version-check-params  (atom {})
+    (let [version-check-params  (promise)
           version-check-test-fn (fn [request-values update-server-url]
-                                  (swap! version-check-params #(assoc % :request-values request-values
-                                                                        :update-server-url update-server-url)))]
+                                  (deliver version-check-params
+                                           {:request-values request-values
+                                            :update-server-url update-server-url}))]
       (with-redefs
        [version-check/check-for-updates! version-check-test-fn]
         (logutils/with-test-logging
@@ -51,5 +60,8 @@
                      :name {:group-id "puppets"
                             :artifact-id "yoda"}
                      :check-for-updates false}}
-          (is (= nil (get-in @version-check-params [:request-values :product-name])))
-          (is (= nil (:update-server-url @version-check-params)))))))))
+          (is (logutils/logged?
+               #"Not checking for updates - opt-out setting exists" :info))
+          (let [params-received (deref version-check-params 100
+                                       :no-params-received-before-time-out-reached)]
+            (is (= :no-params-received-before-time-out-reached params-received)))))))))
