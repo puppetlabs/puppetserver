2a39f306bda067b35f2c93b564bf047cbcbdfc76
Jeremy Barlow <jeremy.barlow@puppet.com>
2017-03-17 15:15:15 -0700

    (SERVER-1264) Configure Graphite max_creates_per_min to infinite
    Previously, when the collect_default_metrics.rb test was configuring
    Graphite, the default Graphite installation was implicitly setting the
    MAX_CREATES_PER_MINUTE setting to 50 (by virtue of the default that the
    Graphite Puppet module was using).  This caused the total time for
    Puppet Server to intially create all metrics to take up to 3 minutes, as
    measured when run in the Puppet vmpooler.
    
    This commit adds an extra test step which configures
    MAX_CREATES_PER_MINUTE to 'inf' (infinite).  This allows the initial
    creation of metrics in the Graphite server to typically take less than 2
    seconds, allowing about 3 minutes to be shaved off the run time for the
    collect_default_metrics.rb test.

--- acceptance/suites/tests/metrics/collect_default_metrics.rb
+++ acceptance/suites/tests/metrics/collect_default_metrics.rb
@@ -41,6 +41,18 @@ step 'Install graphite (and grafana)' do
 
   on(graphite, "puppet module install cprice404-grafanadash --codedir #{tmp_module_dir}")
   on(graphite, "puppet apply -e \"include grafanadash::dev\" --codedir #{tmp_module_dir}")
+
+  # Set max_creates_per_minute to 'inf' (in place of smaller default) in order
+  # to speed up how quickly Graphite makes newly-created metrics available to
+  # be queried - and, therefore, hopefully make this test run much more quickly.
+  graphitepp = "#{tmp_module_dir}/graphite.pp"
+  create_remote_file(master, graphitepp, <<GRAPHITEPP)
+class { 'graphite':
+  gr_web_cors_allow_from_all => true,
+  gr_max_creates_per_minute => inf,
+}
+GRAPHITEPP
+  on(graphite, "puppet apply #{graphitepp} --codedir #{tmp_module_dir}")
 end
 
 step 'Enable graphite and profiler metrics in puppetserver configuration' do
