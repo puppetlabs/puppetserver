85574aa1ac0d8fee35bc3ae2291295ae6229f68e
Adrien Thebo <git@somethingsinistral.net>
2017-05-10 12:58:30 -0700

    (maint) use beaker helper method default values
    The beaker helper `install_puppetlabs_dev_repo` will provide a temp
    configuration directory if one is given, for the sake of simplicity we
    can use that default instead of providing our own.

--- acceptance/suites/pre_suite/foss/30_install_dev_repos.rb
+++ acceptance/suites/pre_suite/foss/30_install_dev_repos.rb
@@ -1,13 +1,12 @@
 install_opts = options.merge( { :dev_builds_repos => ["PC1"] })
-repo_config_dir = 'tmp/repo_configs'
 
 case test_config[:puppetserver_install_type]
 when :package
   step "Setup Puppet Server repositories." do
     package_build_version = ENV['PACKAGE_BUILD_VERSION']
     if package_build_version
-      install_puppetlabs_dev_repo master, 'puppetserver', package_build_version,
-                                  repo_config_dir, install_opts
+      install_puppetlabs_dev_repo(master, 'puppetserver', package_build_version,
+                                  nil, install_opts)
     else
       abort("Environment variable PACKAGE_BUILD_VERSION required for package installs!")
     end
@@ -19,8 +18,8 @@ if puppet_build_version
   confine_block :except, :platform => ['windows'] do
     step "Setup Puppet Labs Dev Repositories." do
       hosts.each do |host|
-        install_puppetlabs_dev_repo host, 'puppet-agent', puppet_build_version,
-                                    repo_config_dir, install_opts
+        install_puppetlabs_dev_repo(host, 'puppet-agent', puppet_build_version,
+                                    nil, install_opts)
       end
     end
   end
