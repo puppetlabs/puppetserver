4d52aab3e894125cf9458bb4d7d827b801fd6d5e
Adrien Thebo <git@somethingsinistral.net>
2017-05-10 13:16:16 -0700

    (maint) Clean up puppet3 compat puppetserver setup
    Since we don't support Puppetserver running on windows we can strip out
    an extraneous confine meant for agents.

--- acceptance/suites/pre_suite/puppet3_compat/30_install_dev_repos.rb
+++ acceptance/suites/pre_suite/puppet3_compat/30_install_dev_repos.rb
@@ -14,12 +14,9 @@ when :package
 
   puppet_build_version = test_config[:puppet_build_version]
   if puppet_build_version
-    confine_block :except, :platform => ['windows'] do
-      step "Setup Puppet dev repository on the master." do
-        install_puppetlabs_dev_repo(master, 'puppet-agent', puppet_build_version,
-                                    repo_config_dir, install_opts)
-      end
+    step "Setup Puppet dev repository on the master." do
+      install_puppetlabs_dev_repo(master, 'puppet-agent', puppet_build_version,
+                                  repo_config_dir, install_opts)
     end
   end
 end
-
