208a2749617d89132a7debdfac379b4322b788b6
Adrien Thebo <git@somethingsinistral.net>
2017-05-10 13:42:55 -0700

    (maint) reorder puppetserver dev repo setup for clarity

--- acceptance/suites/pre_suite/puppet3_compat/30_install_dev_repos.rb
+++ acceptance/suites/pre_suite/puppet3_compat/30_install_dev_repos.rb
@@ -1,19 +1,16 @@
-install_opts = options.merge( { :dev_builds_repos => ["PC1"] })
 
-case test_config[:puppetserver_install_type]
-when :package
+if test_config[:puppetserver_install_type] == :package
+  package_build_version = ENV['PACKAGE_BUILD_VERSION']
+  if package_build_version.nil?
+    abort("Environment variable PACKAGE_BUILD_VERSION required for package installs!")
+  end
+
   step "Setup Puppet Server dev repository on the master." do
-    package_build_version = ENV['PACKAGE_BUILD_VERSION']
-    if package_build_version
-      install_puppetlabs_dev_repo(master, 'puppetserver', package_build_version,
-                                  nil, install_opts)
-    else
-      abort("Environment variable PACKAGE_BUILD_VERSION required for package installs!")
-    end
+    install_opts = options.merge({ :dev_builds_repos => ["PC1"]})
+    install_puppetlabs_dev_repo(master, 'puppetserver', package_build_version, nil, install_opts)
   end
 
   step "Setup Puppet dev repository on the master." do
-    install_puppetlabs_dev_repo(master, 'puppet-agent', test_config[:puppet_build_version],
-                                repo_config_dir, install_opts)
+    install_puppet_agent_dev_repo_on(master, {:puppet_agent_version => test_config[:puppet_build_version]})
   end
 end
