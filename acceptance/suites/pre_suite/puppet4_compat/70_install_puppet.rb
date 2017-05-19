16b0efcf6c0c4d2e2f7fb6b11e889a68c3c979c7
Adrien Thebo <git@somethingsinistral.net>
2017-05-19 15:23:41 -0700

    (SERVER-1786) Reenable setting puppet 4 legacy version

--- acceptance/suites/pre_suite/puppet4_compat/70_install_puppet.rb
+++ acceptance/suites/pre_suite/puppet4_compat/70_install_puppet.rb
@@ -2,10 +2,9 @@ require 'puppetserver/acceptance/compat_utils'
 
 step "Install Legacy Puppet Agents."
 
-puppet_agent_version = '1.9.3'
-#puppet_version = ENV['PUPPET_LEGACY_VERSION']
-#if not puppet_version
-#  fail "PUPPET_LEGACY_VERSION is not set, e.g. '1.9.3'"
-#end
+puppet_version = ENV['PUPPET_LEGACY_VERSION']
+if not puppet_version
+  fail "PUPPET_LEGACY_VERSION is not set, e.g. '1.9.3'"
+end
 
 install_puppet_agent_on(nonmaster_agents, {:puppet_agent_version => puppet_agent_version})
