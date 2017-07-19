d98c9b34998e4e0d083fac17a2bb01fae6e56153
Jenkins CI <ci@puppetlabs.com>
2017-07-19 19:03:28 +0000

    (maint) update puppet submodule version and pins

--- acceptance/config/beaker/options.rb
+++ acceptance/config/beaker/options.rb
@@ -1,14 +1,15 @@
-{
-  :forge_host => 'forge-aio01-petest.puppetlabs.com',
-  :is_puppetserver => true,
-  :is_jvm_puppet => true,
-  "service-wait" => true,
-  "service-prefix" => 'service ',
-  "service-num-retries" => 1500,
-  "puppetservice" => 'puppetserver',
-  "puppetserver-package" => 'puppetserver',
-  "use-service" => true,
-  "master-start-curl-retries" => 60,
-  "puppetserver-confdir" => '/etc/puppetlabs/puppetserver/conf.d',
-  "puppetserver-config" => '/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf'
-}
+{:forge_host=>"forge-aio01-petest.puppetlabs.com",
+ :is_puppetserver=>true,
+ :is_jvm_puppet=>true,
+ "service-wait"=>true,
+ "service-prefix"=>"service ",
+ "service-num-retries"=>1500,
+ "puppetservice"=>"puppetserver",
+ "puppetserver-package"=>"puppetserver",
+ "use-service"=>true,
+ "master-start-curl-retries"=>60,
+ "puppetserver-confdir"=>"/etc/puppetlabs/puppetserver/conf.d",
+ "puppetserver-config"=>
+  "/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf",
+ :puppet_version=>"5.0.0.210.g257e147",
+ :puppet_build_version=>"770784afd62ddb4d7abec323c11bf6a44d9aba31"}
