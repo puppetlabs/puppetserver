bdb6adf86f5dd2bcb38e001563a091139b6bb05f
Adrien Thebo <git@somethingsinistral.net>
2017-08-11 11:40:40 -0700

    (SERVER-1315) Update clj-parent version to 1.3.1
    This change picks up puppetlabs/ssl-utils 0.9.0 for the pem->ca-cert
    function.

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
 
   :min-lein-version "2.7.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "1.2.1"]
+  :parent-project {:coords [puppetlabs/clj-parent "1.3.1"]
                    :inherit [:managed-dependencies]}
 
   :dependencies [[org.clojure/clojure]
