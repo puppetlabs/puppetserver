274120ae762ea18e466a9689ab47e415f2a4cdea
Adrien Thebo <git@somethingsinistral.net>
2017-09-25 13:57:27 -0700

    (SERVER-1922) Set puppet log level from logback
    In the early days of Puppetserver, the Puppet log level was maxed out in
    order to pass all filtering control to logback. This approach didn't
    take into account the potential cost of generating log messages and in
    the real world can have performance implications.
    
    This pull request resolves this problem by using the logback
    puppetserver log level to set the Puppet log level. Because there is not
    a 1:1 mapping of logback levels to Puppet levels the notice log level
    cannot be set; if notice level information is needed then the info level
    must be set.

--- spec/puppet-server-lib/puppet/jvm/puppet_config_spec.rb
+++ spec/puppet-server-lib/puppet/jvm/puppet_config_spec.rb
@@ -3,22 +3,7 @@ require 'puppet/server/puppet_config'
 describe 'Puppet::Server::PuppetConfig' do
   context "When puppet has had settings initialized" do
     before :each do
-      mock_puppet_config = {}
-      Puppet::Server::PuppetConfig.initialize_puppet(mock_puppet_config)
-    end
-
-    describe "the puppet log level (Puppet[:log_level])" do
-      subject { Puppet[:log_level] }
-      it 'is set to debug (the highest) so messages make it to logback' do
-        expect(subject).to eq('debug')
-      end
-    end
-
-    describe "the puppet log level (Puppet::Util::Log.level)" do
-      subject { Puppet::Util::Log.level }
-      it 'is set to debug (the highest) so messages make it to logback' do
-        expect(subject).to eq(:debug)
-      end
+      Puppet::Server::PuppetConfig.initialize_puppet({})
     end
 
     describe '(PUP-5482) Puppet[:always_retry_plugins]' do
