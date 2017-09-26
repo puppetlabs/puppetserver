779fccd16d40718784cb12aa8d0537a7638da626
Adrien Thebo <git@somethingsinistral.net>
2017-09-25 18:11:25 -0700

    (SERVER-1922) Defer settting Puppet log level
    When Puppetserver is booting, it first configures the Ruby/JVM bindings
    which includes tying logback in as a log destination, and then
    configures Puppet. Because Puppet itself is configured after
    Puppetserver it's possible for the default Puppet log level to overwrite
    the logback derived log level. This commit defers the log level
    configuration until Puppet has been initialized to ensure that the
    logback determined level takes precedence.

--- spec/puppet-server-lib/puppet/jvm/puppet_config_spec.rb
+++ spec/puppet-server-lib/puppet/jvm/puppet_config_spec.rb
@@ -1,6 +1,27 @@
 require 'puppet/server/puppet_config'
 
 describe 'Puppet::Server::PuppetConfig' do
+  context "initializing Puppet Server" do
+    context "setting the Puppet log level from logback" do
+      let(:logger) do
+        stub("Logger", isDebugEnabled: false, isInfoEnabled: true, isWarnEnabled: false, isErrorEnabled: false)
+      end
+
+      before :each do
+        Puppet::Server::Logger.expects(:get_logger).returns(logger)
+        Puppet::Server::PuppetConfig.initialize_puppet({})
+      end
+
+      it "the puppet log level (Puppet[:log_level]) is set from logback" do
+        expect(Puppet[:log_level]).to eq('info')
+      end
+
+      it "the puppet log level (Puppet::Util::Log.level) is set from logback" do
+        expect(Puppet::Util::Log.level).to eq(:info)
+      end
+    end
+  end
+
   context "When puppet has had settings initialized" do
     before :each do
       Puppet::Server::PuppetConfig.initialize_puppet({})
