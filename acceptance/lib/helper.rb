cae47beec24765944f08e67591c0f9c0ea85fb7e
Maggie Dreyer <maggie@puppet.com>
2017-10-13 13:19:13 -0700

    (maint) Update puppet and puppet-agent versions for testing
    This updates the puppet submodule from
    fac92d9ae2e1ffde8c8fb274767de53f374da47f to
    b1fa1dd4ab656979b9083ab04f58153ac13a7f45, which is the SHA associated
    with the new puppet-agent version.

--- acceptance/lib/helper.rb
+++ acceptance/lib/helper.rb
@@ -23,7 +23,7 @@ module PuppetServerExtensions
 
     puppet_version = get_option_value(options[:puppet_version],
                          nil, "Puppet Version", "PUPPET_VERSION",
-                         "1.10.8.31.g445a971",
+                         "1.10.8.46.g8b59b88",
                          :string) ||
                          get_puppet_version
 
@@ -32,7 +32,7 @@ module PuppetServerExtensions
     puppet_build_version = get_option_value(options[:puppet_build_version],
                          nil, "Puppet Agent Development Build Version",
                          "PUPPET_BUILD_VERSION",
-                         "445a9718fc68a7e7ebbeb405b28cdffd1a575db6",
+                         "8b59b886d9ce0dc80bfc0cd2ef487924ceb21476",
                          :string)
 
     # puppetdb version corresponds to packaged development version located at:
