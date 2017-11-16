da3acb6d287ece2250b941611665a4d9ffdefa58
Melissa Stone <melissa@puppet.com>
2017-11-15 16:37:30 -0800

    (maint) Update ezbake to 1.6.5

--- project.clj
+++ project.clj
@@ -211,7 +211,7 @@
                                                [puppetlabs/puppetserver ~ps-version :exclusions [puppetlabs/jruby-deps]]
                                                [puppetlabs/trapperkeeper-webserver-jetty9 nil]
                                                [org.clojure/tools.nrepl nil]]
-                      :plugins [[puppetlabs/lein-ezbake "1.6.3"]]
+                      :plugins [[puppetlabs/lein-ezbake "1.6.5"]]
                       :name "puppetserver"}
              :uberjar {:aot [puppetlabs.trapperkeeper.main]
                        :dependencies [[puppetlabs/trapperkeeper-webserver-jetty9 nil]]}
