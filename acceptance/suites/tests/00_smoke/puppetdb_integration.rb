707fc02eeab6fda3842aa400c3c130017e8b4884
Charlie Sharpsteen <source@sharpsteen.net>
2018-01-05 14:56:28 -0800

    (maint) Strengthen assertions in PuppetDB test
    This commit updates the PuppetDB integration test to ensure that the Puppet
    Server process is configured to send data to PuppetDB before the test is run.
    The assertions in the test are also updated to ensure that facts and a catalog
    are submitted in addition to the report and that the timestamps on all
    submitted datasets have been updated. This improves upon the old assertion
    that just ensured the existence of a report --- which could have been submitted
    by a prior test.

--- acceptance/suites/tests/00_smoke/puppetdb_integration.rb
+++ acceptance/suites/tests/00_smoke/puppetdb_integration.rb
@@ -6,9 +6,8 @@
 ## agent run reports.
 ##
 ## This test validates communication is successful by querying the PuppetDB HTTP
-## API and asserting the agent's report timestamp is not null. This means that
-## PuppetDB successfully received the agent's report sent from Puppet Server.
-## We can just run the agent that's on the master for this.
+## API and asserting that an updated factset, catalog and report from an agent
+## run made it into PuppetDB.
 #
 
 # We only run this test if we'll have puppetdb installed, which is gated in
@@ -17,16 +16,34 @@ matching_puppetdb_platform = puppetdb_supported_platforms.select { |r| r =~ mast
 skip_test unless matching_puppetdb_platform.length > 0
 
 require 'json'
+require 'time'
 
-step 'Submit agent report to PuppetDB via server' do
-  with_puppet_running_on(master, {}) do
+run_timestamp = nil
+
+with_puppet_running_on(master, {}) do
+  step 'Enable PuppetDB' do
+    apply_manifest_on(master, <<EOM)
+class{'puppetdb::master::config':
+  enable_reports          => true,
+  manage_report_processor => true,
+}
+EOM
+  end
+
+  step 'Run agent to trigger data submission to PuppetDB' do
+    run_timestamp = Time.now.utc
     on(master, puppet_agent("--test --server #{master}"), :acceptable_exit_codes => [0,2])
   end
 end
 
-step 'Validate server sent agent report to PuppetDB' do
+step 'Validate server sent agent data to PuppetDB' do
   fqdn = on(master, '/opt/puppetlabs/bin/facter fqdn').stdout.chomp
   query = "curl http://localhost:8080/pdb/query/v4/nodes/#{fqdn}"
   response = JSON.parse(on(master, query).stdout.chomp)
-  assert(response['report_timestamp'] != nil)
+  %w[facts_timestamp catalog_timestamp report_timestamp].each do |dataset|
+    assert_operator(Time.iso8601(response[dataset]),
+                    :>,
+                    run_timestamp,
+                   "#{dataset} updated in PuppetDB")
+  end
 end
