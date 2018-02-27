1019458ac68e347959181306af975dd4d8c9eb3a
Matthaus Owens <matthaus@puppet.com>
2018-02-27 14:59:55 -0800

    (maint) Run tests against jruby 9k as well as 1.7
    Previously we would only run travis and specs against the default jruby.
    This is undesirable as we would like to catch test failures against 9k
    before merging, so this commit adds a matrix to the travis configuration
    to run against both jruby versions. The Rakefile is also updated to
    support running specs against jruby9k.

--- ext/travisci/test.sh
+++ ext/travisci/test.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-export PUPPETSERVER_HEAP_SIZE=5G
+export PUPPETSERVER_HEAP_SIZE=4G
 
 echo "Using heap size: $PUPPETSERVER_HEAP_SIZE"
 echo "Total memory available: $(grep MemTotal /proc/meminfo | awk '{print $2}')"
 
-lein test :all
+if [ "${JRUBY_VERSION}" = "1.7" ]; then
+  echo "Running tests with default JRuby (1.7-based)"
+  lein -U test :all
+elif [ "${JRUBY_VERSION}" = "9k" ]; then
+  echo "Running tests with JRuby 9k"
+  lein -U with-profile +jruby9k test :all
+fi
 
-rake spec
+rake spec JRUBY_VERSION=${JRUBY_VERSION}
