9f1dc4e765351454ad4412893f8602f25cf5d284
Morgan Rhodes <morgan@puppet.com>
2018-05-29 16:18:24 -0700

    (maint) Configure autosign setting via environment variable
    Change from https://github.com/puppetlabs/puppet-in-docker/pull/64

--- docker/puppetserver-standalone/docker-entrypoint.sh
+++ docker/puppetserver-standalone/docker-entrypoint.sh
@@ -7,4 +7,10 @@ if test -n "${PUPPETDB_SERVER_URLS}" ; then
   sed -i "s@^server_urls.*@server_urls = ${PUPPETDB_SERVER_URLS}@" /etc/puppetlabs/puppet/puppetdb.conf
 fi
 
+# Configure puppet to use a certificate autosign script (if it exists)
+# AUTOSIGN=true|false|path_to_autosign.conf
+if [[ -n "${AUTOSIGN}" ]]; then
+  puppet config set autosign "$AUTOSIGN" --section master
+fi
+
 exec /opt/puppetlabs/bin/puppetserver "$@"
