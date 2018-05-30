7a32195eb49f0f1d5a744f3025868972a5c16d29
Justin Stoller <justin.stoller@gmail.com>
2018-05-30 15:48:51 -0700

    (SERVER-2172) Use puppetserver-ca subcommand & lib
    Adds a puppetserver subcommand `ca` that loads a pre-release version of
    the puppetserver-ca-cli gem.
    
    Conflicts:
      - dev-setup
      - dev/install-test-gems.sh
      - resources/ext/build-scripts/install-vendored-gems.sh

--- resources/ext/build-scripts/install-vendored-gems.sh
+++ resources/ext/build-scripts/install-vendored-gems.sh
@@ -11,7 +11,8 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-cat "${DIR}/gem-list.txt"
+echo "Installing JRuby vendor gems"
+cat "${DIR}/jruby-gem-list.txt"
 
 echo "jruby-puppet: { gem-home: ${DESTDIR}/opt/puppetlabs/server/data/puppetserver/vendored-jruby-gems }" > jruby.conf
 
@@ -21,5 +22,20 @@ do
   gem_name=$(echo $LINE |awk '{print $1}')
   gem_version=$(echo $LINE |awk '{print $2}')
   gem_list+=("$gem_name:$gem_version")
-done < "${DIR}/gem-list.txt"
-java -cp puppet-server-release.jar:jruby-1_7.jar clojure.main -m puppetlabs.puppetserver.cli.gem --config jruby.conf -- install --no-ri --no-rdoc "${gem_list[@]}"
+done < "${DIR}/jruby-gem-list.txt"
+java -cp puppet-server-release.jar:jruby-9k.jar clojure.main -m puppetlabs.puppetserver.cli.gem --config jruby.conf -- install --no-ri --no-rdoc "${gem_list[@]}"
+
+
+echo "Installing MRI vendor gems"
+cat "${DIR}/mri-gem-list.txt"
+
+echo "jruby-puppet: { gem-home: ${DESTDIR}/opt/puppetlabs/puppet/lib/ruby/vendor_gems }" > jruby.conf
+
+gem_list=()
+while read LINE
+do
+  gem_name=$(echo $LINE |awk '{print $1}')
+  gem_version=$(echo $LINE |awk '{print $2}')
+  gem_list+=("$gem_name:$gem_version")
+done < "${DIR}/mri-gem-list.txt"
+java -cp puppet-server-release.jar:jruby-9k.jar clojure.main -m puppetlabs.puppetserver.cli.gem --config jruby.conf -- install --no-ri --no-rdoc "${gem_list[@]}"
