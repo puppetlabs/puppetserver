3aae9386537973778c42309fc397495598ee6e3b
Justin Stoller <justin.stoller@gmail.com>
2018-06-01 14:50:46 -0700

    (maint) Refactor dev/test gem installation
    This commit moves us to using the much faster gem installation that RE
    was using during actual packaging. It also moves the
    `install-test-gets` script into the dev directory since it's a
    development utility and updates `./dev-setup` to call it so there's only
    one script call needed to setup a working dev environment.

---
+++ dev/install-test-gems.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+VENDORED_GEMS_HOME="./target/jruby-gem-home"
+
+gem_list=()
+while read LINE
+do
+  gem_name=$(echo $LINE |awk '{print $1}')
+  gem_version=$(echo $LINE |awk '{print $2}')
+  gem_list+=("$gem_name:$gem_version")
+done < ./resources/ext/build-scripts/gem-list.txt
+
+echo "Installing vendored gems to '${VENDORED_GEMS_HOME}'"
+lein gem install --install-dir "${VENDORED_GEMS_HOME}" --no-ri --no-rdoc "${gem_list[@]}"
