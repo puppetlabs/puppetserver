bc284a9e8c8471af42ad6931a4ec501bbe0fcbf8
Jenkins CI <ci@puppet.com>
2018-06-02 03:38:45 +0000

    (maint) update puppet submodule version and agent pin

--- acceptance/config/beaker/options.rb
+++ acceptance/config/beaker/options.rb
@@ -11,4 +11,4 @@
  "puppetserver-confdir"=>"/etc/puppetlabs/puppetserver/conf.d",
  "puppetserver-config"=>
   "/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf",
- :puppet_build_version=>"4a3f58cfdf122a7dc9a0cf4e0be9ef697c494d0c"}
+ :puppet_build_version=>"9e196321739570a6a90ddc99560fa7d15a8ce2a6"}
