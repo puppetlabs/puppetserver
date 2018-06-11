d13224b301f02775355c780fc78972cc7eca4ba6
Morgan Rhodes <morgan@puppet.com>
2018-06-11 13:11:36 -0700

    (maint) Address bash style comment

--- docker/puppetserver-standalone/docker-entrypoint.sh
+++ docker/puppetserver-standalone/docker-entrypoint.sh
@@ -9,7 +9,7 @@ fi
 
 # Configure puppet to use a certificate autosign script (if it exists)
 # AUTOSIGN=true|false|path_to_autosign.conf
-if [[ -n "${AUTOSIGN}" ]]; then
+if test -n "${AUTOSIGN}" ; then
   puppet config set autosign "$AUTOSIGN" --section master
 fi
 
