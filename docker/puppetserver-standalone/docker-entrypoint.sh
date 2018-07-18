2918eae7a228eff4d3b99f54ee5e61a2f371f985
Morgan Rhodes <morgan@puppet.com>
2018-07-18 11:30:57 -0700

    (maint) Add the ability to set the puppetserver hostname to something other than 'puppet'

--- docker/puppetserver-standalone/docker-entrypoint.sh
+++ docker/puppetserver-standalone/docker-entrypoint.sh
@@ -7,6 +7,11 @@ if test -n "${PUPPETDB_SERVER_URLS}" ; then
   sed -i "s@^server_urls.*@server_urls = ${PUPPETDB_SERVER_URLS}@" /etc/puppetlabs/puppet/puppetdb.conf
 fi
 
+if test -n "${PUPPETSERVER_HOSTNAME}"; then
+  /opt/puppetlabs/bin/puppet config set certname "$PUPPETSERVER_HOSTNAME"
+  /opt/puppetlabs/bin/puppet config set server "$PUPPETSERVER_HOSTNAME"
+fi
+
 # Configure puppet to use a certificate autosign script (if it exists)
 # AUTOSIGN=true|false|path_to_autosign.conf
 if test -n "${AUTOSIGN}" ; then
