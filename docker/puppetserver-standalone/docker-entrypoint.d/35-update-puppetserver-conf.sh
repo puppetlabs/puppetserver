6b4d548a601351e4edd134554eb66b09b3d48b68
Morgan Rhodes <morgan@puppet.com>
2018-12-11 16:42:24 -0800

    (CPR-650) Update default for `max-active-instances`
    Since we default to starting the puppetserver with `-Xmx256m -Xms256m`,
    default to 1 for the max number of active instances.

--- docker/puppetserver-standalone/docker-entrypoint.d/35-update-puppetserver-conf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if test -n "${PUPPETSERVER_MAX_ACTIVE_INSTANCES}"; then
-  sed -i "s/#max-active-instances/max-active-instances/" /etc/puppetlabs/puppetserver/conf.d/puppetserver.conf
-fi
