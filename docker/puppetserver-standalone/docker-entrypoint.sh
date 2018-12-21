9e70ed7a7f0407fd16a94c9364aa9252a60b38ff
Morgan Rhodes <morgan@puppet.com>
2018-12-21 15:42:58 -0800

    (maint) Add the ability to load extra entrypoint scripts at runtime
    This is driven by our need to run some extra startup configuration when
    loadbalancing compile masters with HAProxy. The
    `/docker-entrypoint-additional.d` directory has been added and any `.sh`
    files mounted there will be executed on startup.

--- docker/puppetserver-standalone/docker-entrypoint.sh
+++ docker/puppetserver-standalone/docker-entrypoint.sh
@@ -3,9 +3,24 @@
 set -e
 
 for f in /docker-entrypoint.d/*.sh; do
-    echo "Running $f"
-    chmod +x "$f"
-    "$f"
+  echo "Running $f"
+  chmod +x "$f"
+  "$f"
 done
+
+if [[ -n "$(ls -A /docker-entrypoint-additional.d)" ]]; then
+  for f in /docker-entrypoint-additional.d/*; do
+    case "$f" in
+      *.sh)
+        echo "Running $f"
+        chmod +x "$f"
+        "$f"
+        ;;
+      *)
+        echo "ignoring $f, it's missing a .sh extension"
+        ;;
+    esac
+  done
+fi
 
 exec /opt/puppetlabs/bin/puppetserver "$@"
