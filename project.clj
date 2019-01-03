d1de1dc8cf0e55cb803ef2668b783419e74d094f
Maggie Dreyer <maggie@puppet.com>
2019-01-03 15:08:05 -0800

    (SERVER-2418) Update clj-parent to 1.7.19
    This commit updates clj-parent to 1.7.19, which pulls in
    jackson-databind 2.9.8, fixing several security issues.

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
 
   :min-lein-version "2.7.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "1.7.18"]
+  :parent-project {:coords [puppetlabs/clj-parent "1.7.19"]
                    :inherit [:managed-dependencies]}
 
   :dependencies [[org.clojure/clojure]
