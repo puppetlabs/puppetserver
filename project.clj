e0c0dff04d96c9badcf4bd1f86fadcdffcb87d64
Maggie Dreyer <maggie@puppet.com>
2019-01-08 15:35:04 -0800

    (SERVER-2416) Update ezbake to 1.9.2

--- project.clj
+++ project.clj
@@ -177,7 +177,7 @@
                                                [puppetlabs/puppetserver ~ps-version :exclusions [puppetlabs/jruby-deps]]
                                                [puppetlabs/trapperkeeper-webserver-jetty9 nil]
                                                [org.clojure/tools.nrepl nil]]
-                      :plugins [[puppetlabs/lein-ezbake "1.8.10"]]
+                      :plugins [[puppetlabs/lein-ezbake "1.9.2"]]
                       :name "puppetserver"}
              :uberjar {:aot [puppetlabs.trapperkeeper.main]
                        :dependencies [[puppetlabs/trapperkeeper-webserver-jetty9 nil]]}
