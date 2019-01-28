9c30938c2b23f4466c6229919c7862ab14669c86
Morgan Rhodes <morgan@puppet.com>
2019-01-28 11:55:18 -0800

    (maint) Add tests for puppetserver without CA

--- docker/spec/puppetserver_spec.rb
+++ docker/spec/puppetserver_spec.rb
@@ -28,10 +28,20 @@ describe 'puppetserver container' do
                --name puppet.test \
                --hostname puppet.test \
                #{@image}).chomp
+    @compiler = %x(docker run --rm --detach \
+               --env DNS_ALT_NAMES=puppet \
+               --env PUPPERWARE_DISABLE_ANALYTICS=true \
+               --env ENABLE_CA=false \
+               --env CA_HOSTNAME=puppet.test \
+               --link puppet.test \
+               --name puppet-compiler.test \
+               --hostname puppet-compiler.test \
+               #{@image}).chomp
   end
 
   after(:all) do
     %x(docker container kill #{@container}) unless @container.nil?
+    %x(docker container kill #{@compiler}) unless @compiler.nil?
   end
 
   it 'should start puppetserver successfully' do
@@ -48,6 +58,25 @@ describe 'puppetserver container' do
 
   it 'should be able to run a puppet agent against the puppetserver' do
     output = %x(docker run --rm --name puppet-agent.test --hostname puppet-agent.test --link puppet.test puppet/puppet-agent-alpine:latest agent --test --server puppet.test)
+    status = $?.exitstatus
+    puts output
+    expect(status).to eq(0)
+  end
+
+  it 'should be able to start a compile master' do
+    status = puppetserver_health_check(@compiler)
+    while (status == 'starting' || status == "'starting'")
+      sleep(1)
+      status = puppetserver_health_check(@compiler)
+    end
+    if status !~ /\'?healthy\'?/
+      puts %x(docker logs #{@compiler})
+    end
+    expect(status).to match(/\'?healthy\'?/)
+end
+
+  it 'should be able to run an agent against the compile master' do
+    output = %x(docker run --rm --name puppet-agent-compiler.test --hostname puppet-agent-compiler.test --link puppet.test --link puppet-compiler.test puppet/puppet-agent-alpine:latest agent --test --server puppet-compiler.test --ca_server puppet.test)
     status = $?.exitstatus
     puts output
     expect(status).to eq(0)
