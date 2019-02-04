8bc1ea126203541e01810549611d5d83c4fc6980
Morgan Rhodes <morgan@puppet.com>
2019-02-04 14:29:10 -0800

    (maint) If consul is enabled, register the puppet service on startup

---
+++ docker/puppetserver-standalone/docker-entrypoint.d/90-consul.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ "$CONSUL_ENABLED" = "true" ]; then
+  ipaddress="$(ifconfig eth0 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+  consul_hostname="${CONSUL_HOSTNAME:-consul}"
+  consul_port="${CONSUL_PORT:-8500}"
+
+  cat <<SERVICEDEF > /puppet-service.json
+{
+  "name": "puppet",
+  "id": "$HOSTNAME",
+  "port": 8140,
+  "address": "$ipaddress",
+  "checks": [
+    {
+      "http": "https://$HOSTNAME:8140/production/status/test",
+      "tls_skip_verify": true,
+      "header": {"Accept": ["pson"]},
+      "interval": "30s",
+      "deregister_critical_service_after": "5m"
+    }
+  ]
+}
+SERVICEDEF
+
+  curl \
+    --request PUT \
+    --data @puppet-service.json \
+    http://$consul_hostname:$consul_port/v1/agent/service/register
+fi
