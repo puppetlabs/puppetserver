88e7d7a82958fb11e8b875d5ba916c5b13c44537
Maggie Dreyer <maggie@puppet.com>
2019-02-08 08:49:44 -0800

    (SERVER-2444) Add fact processing to custom catalog compile
    This commit adds more robust fact processing to the flexible catalog
    compilation code. It fetches facts from PDB if they are not present in
    the request, sanitizes all fact values, and separates out trusted facts.
    This commit also adds server facts to the node object.

--- src/ruby/puppetserver-lib/puppet/server/master.rb
+++ src/ruby/puppetserver-lib/puppet/server/master.rb
@@ -4,6 +4,7 @@ require 'puppet/info_service'
 
 require 'puppet/network/http'
 require 'puppet/network/http/api/master/v3'
+require 'puppet/node/facts'
 
 require 'puppet/server/config'
 require 'puppet/server/puppet_config'
@@ -35,6 +36,7 @@ class Puppet::Server::Master
                           chain(Puppet::Network::HTTP::API::Master::V3.routes)
     register([master_routes])
     @env_loader = Puppet.lookup(:environments)
+    set_server_facts
   end
 
   def handleRequest(request)
@@ -60,8 +62,37 @@ class Puppet::Server::Master
         response["X-Puppet-Version"])
   end
 
-  def compileCatalog(_request_data)
-    {}
+  def convert_java_args_to_ruby(hash)
+    Hash[hash.collect do |key, value|
+        # Stolen and modified from params_to_ruby in handler.rb
+        newkey = key.to_s
+        newkey.slice!(0)
+        if value.java_kind_of?(Java::ClojureLang::PersistentArrayMap)
+          [newkey, convert_java_args_to_ruby(value)]
+        else
+          [newkey, value.java_kind_of?(Java::JavaUtil::List) ? value.to_a : value]
+        end
+      end]
+  end
+
+  def compileCatalog(request_data)
+    processed_hash = convert_java_args_to_ruby(request_data)
+
+    facts, trusted_facts = process_facts(processed_hash)
+    node_params = { facts: facts,
+                    environment: processed_hash['environment'],
+                    # Are these 'parameters' the same as what Node expects?
+                    # There's a bunch of code in Node around merging additional things,
+                    # notably facts, into the 'parameter' field. Is that necessary? If so,
+                    # why?
+                    parameters: processed_hash['parameters'],
+                    classes: processed_hash['classes'] }
+
+    node = Puppet::Node.new(processed_hash['certname'], node_params)
+    node.trusted_data = trusted_facts
+    node.add_server_facts(@server_facts)
+    catalog = Puppet::Parser::Compiler.compile(node, processed_hash['job_id'])
+    catalog.to_data_hash
   end
 
   def getClassInfoForEnvironment(env)
@@ -126,6 +157,73 @@ class Puppet::Server::Master
   end
 
   private
+
+  # @return Puppet::Node::Facts facts, Hash trusted_facts
+  def process_facts(request_data)
+    if request_data['facts'].nil?
+      facts = get_facts_from_pdb(request_data['certname'], request_data['environment'])
+    else
+      facts_from_request = request_data['facts']
+      facts_from_request['name'] = request_data['certname']
+      facts = Puppet::Node::Facts.from_data_hash(facts_from_request)
+    end
+
+    fact_values = if facts.nil?
+                    {}
+                  else
+                    facts.sanitize
+                    facts.to_data_hash
+                  end
+
+    # Pull the trusted facts from the request, or attempt to extract them from
+    # the facts hash
+    trusted_facts = if request_data['trusted_facts'].nil?
+                      fact_values['trusted'].nil? ? {} : fact_values['trusted']
+                    else
+                      request_data['trusted_facts']['values']
+                    end
+
+    return facts, trusted_facts
+  end
+
+  def get_facts_from_pdb(nodename, environment)
+    if Puppet::Node::Facts.indirection.terminus_class == :puppetdb
+      Puppet::Node::Facts.indirection.find(name, :environment => environment)
+    else
+      # How should this be surfaced? Seems like we could maybe do better than a 500, unless
+      # that's accurate?
+      raise(Puppet::Error, "PuppetDB not configured, please provide facts with your catalog request.")
+    end
+  end
+
+  # Initialize our server fact hash; we add these to each client, and they
+  # won't change while we're running, so it's safe to cache the values.
+  def set_server_facts
+    @server_facts = {}
+
+    # Add our server version to the fact list
+    @server_facts['serverversion'] = Puppet.version.to_s
+
+    # And then add the server name and IP
+    { 'servername' => 'fqdn',
+      'serverip' => 'ipaddress'
+    }.each do |var, fact|
+      if value = Facter.value(fact)
+        @server_facts[var] = value
+      else
+        Puppet.warning "Could not retrieve fact #{fact}"
+      end
+    end
+
+    if @server_facts['servername'].nil?
+      host = Facter.value(:hostname)
+      if domain = Facter.value(:domain)
+        @server_facts['servername'] = [host, domain].join('.')
+      else
+        @server_facts['servername'] = host
+      end
+    end
+  end
 
   def self.getModules(env)
     env.modules.collect do |mod|
