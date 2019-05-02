7fb6a93a6de3000a13726cb814fffa6f2f63e040
Mickaël Canévet <mickael.canevet@camptocamp.com>
2019-05-02 10:24:43 +0200

    Fix healthcheck when masterport is different than 8140

--- docker/puppetserver-standalone/healthcheck.sh
+++ docker/puppetserver-standalone/healthcheck.sh
@@ -4,11 +4,12 @@ set -x
 set -e
 
 hostname=$(puppet config print certname) && \
+masterport=$(puppet config print masterport) && \
 curl --fail \
---resolve "${hostname}:8140:127.0.0.1" \
+--resolve "${hostname}:${masterport}:127.0.0.1" \
 --cert   $(puppet config print hostcert) \
 --key    $(puppet config print hostprivkey) \
 --cacert $(puppet config print localcacert) \
-"https://${hostname}:8140/${PUPPET_HEALTHCHECK_ENVIRONMENT}/status/test" \
+"https://${hostname}:${masterport}/${PUPPET_HEALTHCHECK_ENVIRONMENT}/status/test" \
 |  grep -q '"is_alive":true' \
 || exit 1
