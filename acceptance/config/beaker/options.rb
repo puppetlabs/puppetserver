de8f1b6e5b0939430d736e02ebfc9b54f837daf3
Justin Stoller <justin.stoller@gmail.com>
2019-07-12 15:03:38 -0700

    (SERVER-2572) Update to latest puppet-agent in 6.4.3_release

--- acceptance/config/beaker/options.rb
+++ acceptance/config/beaker/options.rb
@@ -11,4 +11,4 @@
  "puppetserver-confdir"=>"/etc/puppetlabs/puppetserver/conf.d",
  "puppetserver-config"=>
   "/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf",
- :puppet_build_version=>"9d0a651271fcd120a2e9197e26a65cba95a92eaf"}
+ :puppet_build_version=>"52b752e982c86b18922f943a3f4487a0bd1ee63e"}
