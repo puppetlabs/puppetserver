3b2b2c983f318807a0569685e3c38d82192b8d50
Tony Vu <vu.tony@gmail.com>
2019-08-14 17:21:47 -0700

    (SERVER-2585) make lookups accessible

--- src/ruby/puppetserver-lib/puppet/server/key_recorder.rb
+++ src/ruby/puppetserver-lib/puppet/server/key_recorder.rb
@@ -2,6 +2,8 @@ require 'puppet/server'
 require 'puppet/pops/lookup/key_recorder'
 
 class Puppet::Server::KeyRecorder < Puppet::Pops::Lookup::KeyRecorder
+  attr_accessor :lookups
+
   def initialize
     @lookups = Hash.new(0)
   end
