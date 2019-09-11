c60c6cc0b1e6249b74a15b1a36808a9d6f2cf723
Morgan Rhodes <morgan@puppet.com>
2019-09-11 11:57:04 -0700

    (maint) Add messages for why we're doing the copies/cleanups

--- docker/puppetserver-standalone/docker-entrypoint.d/20-use-templates-initially.sh
+++ docker/puppetserver-standalone/docker-entrypoint.d/20-use-templates-initially.sh
@@ -14,10 +14,13 @@ cd /
 
 # if the data dir is empty, copy over everything from build time
 if [ ! "$(ls -A /opt/puppetlabs/server/data/puppetserver)" ]; then
+  # https://github.com/moby/moby/issues/39892
+  echo "Loading new empty VOLUME with default /opt/puppetlabs/server/data/puppetserver content (LCOW moby 39892 workaround)"
   cp -R /var/tmp/puppetserver/* /opt/puppetlabs/server/data/puppetserver/
   # remove the tmp dir so we only run this on first runs of new containers
   rm -rf /var/tmp/puppetserver/vendored-jruby-gems
 elif [ -d /var/tmp/puppetserver/vendored-jruby-gems ]; then
+  echo "Upgrading /opt/puppetlabs/server/data/puppetserver/vendored-jruby-gems"
   # clean up existing vendored gems
   rm -rf /opt/puppetlabs/server/data/puppetserver/vendored-jruby-gems
   cp -R /var/tmp/puppetserver/vendored-jruby-gems /opt/puppetlabs/server/data/puppetserver/
