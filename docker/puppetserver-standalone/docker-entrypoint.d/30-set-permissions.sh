730960fec57bc83133bb01bab1d0d0a69fa92d80
Yury Bushmelev <jay4mail@gmail.com>
2019-09-23 13:22:37 +0800

    Add 2 missing directories to fix permissions for
    Following directories are owned by `puppet` user in container. If UID/GID is changed, those directories permissions must be updated too.

--- docker/puppetserver-standalone/docker-entrypoint.d/30-set-permissions.sh
+++ docker/puppetserver-standalone/docker-entrypoint.d/30-set-permissions.sh
@@ -2,3 +2,5 @@
 
 chown -R puppet:puppet /etc/puppetlabs/puppet/
 chown -R puppet:puppet /opt/puppetlabs/server/data/puppetserver/
+chown -R puppet:puppet /etc/puppetlabs/puppetserver/
+chown -R puppet:puppet /var/log/puppetlabs/puppetserver/
