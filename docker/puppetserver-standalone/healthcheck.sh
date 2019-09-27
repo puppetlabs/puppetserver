4c34d3a63d345bf30b906e46cc1067a64ecb3401
Iristyle <Iristyle@github>
2019-09-27 08:58:45 -0700

    (maint) Explicitly set SSLDIR / CERTNAME in Docker
     - This container doesn't follow the same setup as pe-puppetserver and
       these env vars are mostly meaningless
    
       But this allows us to rewrite the healthcheck script more sensibly

--- docker/puppetserver-standalone/healthcheck.sh
+++ docker/puppetserver-standalone/healthcheck.sh
@@ -1,18 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -x
 set -e
 
-certname=$(ls /etc/puppetlabs/puppet/ssl/certs | grep --invert-match ca.pem) && \
-hostname=$(basename $certname .pem) && \
-hostprivkey=/etc/puppetlabs/puppet/ssl/private_keys/$certname && \
-hostcert=/etc/puppetlabs/puppet/ssl/certs/$certname && \
-localcacert=/etc/puppetlabs/puppet/ssl/certs/ca.pem && \
 curl --fail \
---resolve "${hostname}:${PUPPET_MASTERPORT}:127.0.0.1" \
---cert   $hostcert \
---key    $hostprivkey \
---cacert $localcacert \
-"https://${hostname}:${PUPPET_MASTERPORT}/status/v1/simple" \
-|  grep -q '^running$' \
-|| exit 1
+    --resolve "${HOSTNAME}:${PUPPET_MASTERPORT}:127.0.0.1" \
+    --cert    "${SSLDIR}/certs/${CERTNAME}.pem" \
+    --key     "${SSLDIR}/private_keys/${CERTNAME}.pem" \
+    --cacert  "${SSLDIR}/certs/ca.pem" \
+    "https://${HOSTNAME}:${PUPPET_MASTERPORT}/status/v1/simple" \
+    |  grep -q '^running$' \
+    || exit 1
