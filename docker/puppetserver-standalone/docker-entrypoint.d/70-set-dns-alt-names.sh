23b93e924f348a62a28b2a5df9e893fa7fd098c7
Justin Riley <justin.t.riley@gmail.com>
2019-10-15 22:46:28 -0400

    put DNS_ALT_NAMES in agent when CA_ENABLED != true
    A compiler-only instance's cert generation takes place in 80-ca.sh via a
    noop puppet agent run, however, DNS_ALT_NAMES currently gets set in the
    master section of the config which isn't visible to the agent. This
    patch sets DNS_ALT_NAMES within the agent section of the config for
    compiler-only instances (ie when CA_ENABLED != true) so that the initial
    noop agent run includes DNS_ALT_NAMES when requesting the cert.

--- docker/puppetserver-standalone/docker-entrypoint.d/70-set-dns-alt-names.sh
+++ docker/puppetserver-standalone/docker-entrypoint.d/70-set-dns-alt-names.sh
@@ -4,11 +4,17 @@
 # setting will only have an effect when the container is started without
 # an existing certificate on the /etc/puppetlabs/puppet volume
 if test -n "${DNS_ALT_NAMES}"; then
+    config_section="master"
+    if [[ "${CA_ENABLED}" != "true" ]]; then
+      # we are just an ordinary compiler using puppet agent to generate cert
+      config_section="agent"
+    fi
+
     certname=$(puppet config print certname)
     if test ! -f "/etc/puppetlabs/puppet/ssl/certs/$certname.pem" ; then
-        puppet config set dns_alt_names "${DNS_ALT_NAMES}" --section master
+        puppet config set dns_alt_names "${DNS_ALT_NAMES}" --section "${config_section}"
     else
-        actual=$(puppet config print dns_alt_names --section master)
+        actual=$(puppet config print dns_alt_names --section "${config_section}")
         if test "${DNS_ALT_NAMES}" != "${actual}" ; then
             echo "Warning: DNS_ALT_NAMES has been changed from the value in puppet.conf"
             echo "         Remove/revoke the old certificate for this to become effective"
