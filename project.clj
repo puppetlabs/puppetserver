b41823a767e428d732671c76b604447cb07e25f8
Tony Vu <vu.tony@gmail.com>
2019-11-11 16:43:54 -0800

    (maint) Bump clj-parent
    This version includes a jruby-utils update that contains an updated schema
    for JRubyConfig to include a `multithreaded` Boolean.

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "4.2.6"]
+  :parent-project {:coords [puppetlabs/clj-parent "4.2.7"]
                    :inherit [:managed-dependencies]}
 
   :dependencies [[org.clojure/clojure]
