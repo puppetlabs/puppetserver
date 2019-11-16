f58c0108b8974b5f6c70055c29ec863ec96bf68c
Maggie Dreyer <maggie@puppet.com>
2019-11-15 16:30:07 -0800

    (SERVER-2674) Update clj-parent to 4.2.8
    This update downgrades JRuby to 9.2.8.0 to avoid a bug in 9.2.9.0.

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "4.2.7"]
+  :parent-project {:coords [puppetlabs/clj-parent "4.2.8"]
                    :inherit [:managed-dependencies]}
 
   :dependencies [[org.clojure/clojure]
