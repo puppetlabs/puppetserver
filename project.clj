7d1a9ff9af227325e3956214d9abfc12532ecdb5
Maggie Dreyer <maggie@puppet.com>
2020-01-13 08:31:55 -0800

    (maint) Update clj-parent to 4.2.11
    This commit updates some ambiguous type specifications that were causing
    problems on Java 11.

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "4.2.8"]
+  :parent-project {:coords [puppetlabs/clj-parent "4.2.11"]
                    :inherit [:managed-dependencies]}
 
   :dependencies [[org.clojure/clojure]
