7cabd689e8be7c01fef772a5540f10b6a22e4690
Maggie Dreyer <maggie@puppet.com>
2020-03-13 09:13:27 -0700

    (maint) Refactor to reduce repetition

--- acceptance/scripts/generic/testrun.sh
+++ acceptance/scripts/generic/testrun.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+
+do_init()
+{
+  if [ -z "$PACKAGE_BUILD_VERSION" ];
+    #TODO: Read last passing SHA file on builds.pl.d.net
+    then echo "$0: PACKAGE_BUILD_VERSION not set.  Please export PACKAGE_BUILD_VERSION."
+    exit -1;
+  fi
+
+  bundle exec beaker-hostgenerator $GENCONFIG_LAYOUT > $BEAKER_CONFIG
+
+  BEAKER_INIT="bundle exec beaker init --debug"
+  BEAKER_INIT="$BEAKER_INIT --type aio"
+  BEAKER_INIT="$BEAKER_INIT --keyfile $BEAKER_KEYFILE"
+  BEAKER_INIT="$BEAKER_INIT --helper $BEAKER_HELPER"
+  BEAKER_INIT="$BEAKER_INIT --options-file $BEAKER_OPTIONS"
+  BEAKER_INIT="$BEAKER_INIT --post-suite $BEAKER_POSTSUITE"
+  BEAKER_INIT="$BEAKER_INIT --load-path acceptance/lib"
+  BEAKER_INIT="$BEAKER_INIT --hosts $BEAKER_CONFIG"
+  BEAKER_INIT="$BEAKER_INIT --pre-suite $BEAKER_PRESUITE"
+  BEAKER_INIT="$BEAKER_INIT --tests $BEAKER_TESTSUITE"
+  BEAKER_INIT="$BEAKER_INIT --debug --timeout 360"
+  $BEAKER_INIT
+
+  if [ $? != 0 ] ; then
+    echo "Initialization failed!"
+    exit -1;
+  fi
+
+  bundle exec beaker provision
+
+  if [ $? != 0 ] ; then
+    echo "Provision failed!"
+    exit -1;
+  fi
+}
+
 set -x
 
 export GEM_SOURCE="https://artifactory.delivery.puppetlabs.net/artifactory/api/gems/rubygems/"
@@ -13,86 +50,38 @@ export BEAKER_HELPER="${BEAKER_HELPER:-acceptance/lib/helper.rb}"
 
 bundle install --path vendor/bundle
 
-#TODO: Someday, when our Rakefile is refactored, this section will go away
-BEAKER_INIT="bundle exec beaker init --debug"
-BEAKER_INIT="$BEAKER_INIT --type aio"
-BEAKER_INIT="$BEAKER_INIT --keyfile $BEAKER_KEYFILE"
-BEAKER_INIT="$BEAKER_INIT --helper $BEAKER_HELPER"
-BEAKER_INIT="$BEAKER_INIT --options-file $BEAKER_OPTIONS"
-BEAKER_INIT="$BEAKER_INIT --post-suite $BEAKER_POSTSUITE"
-BEAKER_INIT="$BEAKER_INIT --load-path acceptance/lib"
-
 case $1 in
   -p | --p* )
-  if [ -z "$PACKAGE_BUILD_VERSION" ];
-    #TODO: Read last passing SHA file on builds.pl.d.net
-    then echo "$0: PACKAGE_BUILD_VERSION not set.  Please export PACKAGE_BUILD_VERSION."
-    exit -1;
-  fi
+    do_init
 
-  bundle exec beaker-hostgenerator $GENCONFIG_LAYOUT > $BEAKER_CONFIG
+    bundle exec beaker exec
 
-  BEAKER_INIT="$BEAKER_INIT --pre-suite $BEAKER_PRESUITE"
-  BEAKER_INIT="$BEAKER_INIT --tests $BEAKER_TESTSUITE"
-  BEAKER_INIT="$BEAKER_INIT --hosts $BEAKER_CONFIG"
-  $BEAKER_INIT
-  bundle exec beaker provision
+    echo "Preserving hosts, run `bundle exec beaker exec <tests>` to use the same hosts again."
+    ;;
 
-  if [ $? != 0 ] ; then
-    echo "Provision failed!"
-    exit -1;
-  fi
+  -r | --r* )
+    if [ ! -s ./.beaker/subcommand_options.yaml ];
+    then echo "$0: Can not find subcommand_options.yaml; cannot run without init.\n \
+      Use this script with -p to create new hosts and initialize them."
+      exit -1;
+    fi
 
-  bundle exec beaker exec
+    bundle exec beaker exec $BEAKER_TESTSUITE
+    ;;
 
-  echo "Preserving hosts, run `bundle exec beaker exec <tests>` to use the same hosts again."
-  ;;
+  * ) # Preserve hosts on failure
+    do_init
 
+    bundle exec beaker exec
 
--r | --r* )
-  if [ ! -s ./.beaker/subcommand_options.yaml ];
-  then echo "$0: Can not find subcommand_options.yaml; cannot run without init.\n \
-    Use this script with -p to create new hosts and initialize them."
-    exit -1;
-  fi
-
-  bundle exec beaker exec $BEAKER_TESTSUITE
-  ;;
-
-
-* )
-  if [ -z "$PACKAGE_BUILD_VERSION" ];
-    #TODO: Read last passing SHA file on builds.pl.d.net
-    then echo "$0: PACKAGE_BUILD_VERSION not set.  Please export PACKAGE_BUILD_VERSION."
-    exit -1;
-  fi
-
-  bundle exec beaker-hostgenerator $GENCONFIG_LAYOUT > $BEAKER_CONFIG
-
-  # run it with the old options.
-  BEAKER_INIT="$BEAKER_INIT --pre-suite $BEAKER_PRESUITE"
-  BEAKER_INIT="$BEAKER_INIT --hosts $BEAKER_CONFIG"
-  BEAKER_INIT="$BEAKER_INIT --pre-suite $BEAKER_PRESUITE"
-  BEAKER_INIT="$BEAKER_INIT --tests $BEAKER_TESTSUITE"
-  BEAKER_INIT="$BEAKER_INIT --debug --timeout 360"
-  $BEAKER_INIT
-
-  bundle exec beaker provision
-
-  if [ $? != 0 ] ; then
-    echo "Provision failed!"
-    exit -1;
-  fi
-
-  bundle exec beaker exec
-  if [ $? = 0 ] ; then
-    echo "Run completed successfully, destroying hosts."
-    bundle exec beaker destroy
-    rm .beaker/subcommand_options.yaml
-  else
-    echo "Run failed, preserving hosts. Run against these hosts again with `bundle exec beaker exec <tests>`."
-    echo "Alternatively, run this script again with `-r`."
-  fi
-  ;;
+    if [ $? = 0 ] ; then
+      echo "Run completed successfully, destroying hosts."
+      bundle exec beaker destroy
+      rm .beaker/subcommand_options.yaml
+    else
+      echo "Run failed, preserving hosts. Run against these hosts again with `bundle exec beaker exec <tests>`."
+      echo "Alternatively, run this script again with `-r`."
+    fi
+    ;;
 esac
 
