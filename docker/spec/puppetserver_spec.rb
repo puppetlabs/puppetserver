c07b63df321271d9545be46fea277a7c9dda5604
Iristyle <Iristyle@github>
2020-04-16 08:16:45 -0700

    (maint) Remove LCOW support / convert to WSL2
     - Remove having to add .test to the end of container names in compose
       due to DNS resolution bugs in LCOW
     - Remove any LCOW file system copy workarounds given WSL2 does
       not have these problems

--- docker/spec/puppetserver_spec.rb
+++ docker/spec/puppetserver_spec.rb
@@ -18,11 +18,11 @@ end
 
 describe 'puppetserver container' do
   it 'should be able to run a puppet agent against the puppetserver' do
-    expect(run_agent('puppet-agent.test', 'puppetserver_test', masterport: '8141')).to eq(0)
+    expect(run_agent('puppet-agent', 'puppetserver_test', masterport: '8141')).to eq(0)
   end
 
   it 'should be able to run an agent against the compile master' do
-    expect(run_agent('compiler-agent.test', 'puppetserver_test', server: get_container_hostname(get_service_container('compiler')), ca: get_container_hostname(get_service_container('puppet')), ca_port: '8141')).to eq(0)
+    expect(run_agent('compiler-agent', 'puppetserver_test', server: get_container_hostname(get_service_container('compiler')), ca: get_container_hostname(get_service_container('puppet')), ca_port: '8141')).to eq(0)
   end
 
   it 'should have r10k available' do
