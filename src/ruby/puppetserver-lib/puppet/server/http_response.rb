7d3f3aae39bfe07c096fcd77f45c64f53fde61db
Maggie Dreyer <maggie@puppet.com>
2020-09-21 16:39:41 -0700

    (SERVER-2780) Add HttpResponse wrapper
    This commit adds a new HttpResponse class that implements the same API
    as Puppet's HttpResponse, but delegates to the Java apache HTTP client
    instead of Net::HTTP.

---
+++ src/ruby/puppetserver-lib/puppet/server/http_response.rb
@@ -0,0 +1,47 @@
+require 'puppet/http/response'
+
+class Puppet::Server::HttpResponse < Puppet::HTTP::Repsonse
+
+  def initialize(java_response, url)
+    @java_response = java_response
+    @url = url
+  end
+
+  def code
+    @java_response.get_status
+  end
+
+  def reason
+    @java_response.get_reason_phrase
+  end
+
+  def body
+    @java_response.get_body
+  end
+
+  def read_body
+    raise ArgumentError, "A block is required" unless block_given?
+
+    yield body
+  end
+
+  # The HTTP standard counts all codes 200-299 as successes
+  def success?
+    code > 199 && code < 300
+  end
+
+  def [](name)
+    @java_response.get_headers[name]
+  end
+
+  # Yield each header name and value. Returns an enumerator if no block is given.
+  def each_header
+    @java_response.get_headers.each(&block)
+  end
+
+  # Drain the response body
+  def drain
+    body
+    true
+  end
+end
