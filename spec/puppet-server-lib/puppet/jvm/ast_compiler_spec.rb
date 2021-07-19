9cb17ad771039baf91e4fc411a1cac12fb7e0269
Sean P. McDonald <sean.mcdonald@puppet.com>
2021-07-19 10:33:41 -0500

    (PE-32484) Update AST compiler to work for projects
    This commit adds logic to the compile endpoint to support compiling AST for a
    bolt project rather than an environment. Mostly project compiles are the same
    as environment compiles (after all, we are still just compiling puppet code)
    with a few important differences:
    * We pass some additional data about the project to the compile operation
      (this is mostly data about the project name and where it's located on disk)
    * The context of project compiles requires us to use `in_tmp_environment`
      instead of `in_environment` so that we can 1. ensure that we are not loading
      things from environment.conf and 2. that we can control the entirety of the
      modulepath for the compile (projects do not make use of
      basemodulepath/vendormodulepath and we need to ensure we have only exactly
      the modulepath passed in by the project)

--- spec/puppet-server-lib/puppet/jvm/ast_compiler_spec.rb
+++ spec/puppet-server-lib/puppet/jvm/ast_compiler_spec.rb
@@ -15,12 +15,26 @@ def generate_ast(code)
   end
 end
 
-def request(code)
+def environment_request(code)
   {"certname" => "localhost",
    "facts" => {"values" => {"my_fact" => "fact_value"}},
    "trusted_facts" => {"values" => {"my_trusted" => "trusted_value"}},
    "options" => {"capture_logs" => false},
    "environment" => "production",
+   "code_ast" => generate_ast(code).to_json,
+   "variables" => {"values" => {"foo" => "bar"}}}
+end
+
+def project_request(code)
+  {"certname" => "localhost",
+   "facts" => {"values" => {"my_fact" => "fact_value"}},
+   "trusted_facts" => {"values" => {"my_trusted" => "trusted_value"}},
+   "options" => {"capture_logs" => false, "compile_for_plan" => true},
+   "versioned_project" => "fakeproject_GITSHA",
+   "project_root" => "/etc/puppetlabs/puppetserver/projects",
+   "modulepath" => ['/etc/puppetlabs/puppetserver/projects/fakeproject_GITSHA', '/etc/puppetlabs/puppetserver/projects/fakeproject_GITSHA/.modules'],
+   "hiera_config" => ['/etc/puppetlabs/puppetserver/projects/fakeproject_GITSHA/hiera.yaml'],
+   "project_name" => 'fakeproject',
    "code_ast" => generate_ast(code).to_json,
    "variables" => {"values" => {"foo" => "bar"}}}
 end
@@ -37,42 +51,42 @@ describe Puppet::Server::ASTCompiler do
     let(:boltlib_path) { nil }
 
     it 'handles basic resources' do
-      response = Puppet::Server::ASTCompiler.compile(request("notify { 'my_notify': }"), boltlib_path)
+      response = Puppet::Server::ASTCompiler.compile(environment_request("notify { 'my_notify': }"), boltlib_path)
       notify = find_notify(response[:catalog])
       expect(notify).not_to be_nil
       expect(notify['title']).to eq("my_notify")
     end
 
     it 'correctly interpolates supplied variables' do
-      response = Puppet::Server::ASTCompiler.compile(request('notify { "$foo": }'), boltlib_path)
+      response = Puppet::Server::ASTCompiler.compile(environment_request('notify { "$foo": }'), boltlib_path)
       notify = find_notify(response[:catalog])
       expect(notify).not_to be_nil
       expect(notify['title']).to eq("bar")
     end
 
     it 'correctly interpolates supplied facts' do
-      response = Puppet::Server::ASTCompiler.compile(request('notify { "$my_fact": }'), boltlib_path)
+      response = Puppet::Server::ASTCompiler.compile(environment_request('notify { "$my_fact": }'), boltlib_path)
       notify = find_notify(response[:catalog])
       expect(notify).not_to be_nil
       expect(notify['title']).to eq("fact_value")
     end
 
     it 'correctly interpolates supplied trusted facts' do
-      response = Puppet::Server::ASTCompiler.compile(request('notify { "${trusted[\'my_trusted\']}": }'), boltlib_path)
+      response = Puppet::Server::ASTCompiler.compile(environment_request('notify { "${trusted[\'my_trusted\']}": }'), boltlib_path)
       notify = find_notify(response[:catalog])
       expect(notify).not_to be_nil
       expect(notify['title']).to eq("trusted_value")
     end
 
     it 'fails gracefully when boltlib is not found when request requires bolt types' do
-      bolt_request = request('notify { "${trusted[\'my_trusted\']}": }')
+      bolt_request = environment_request('notify { "${trusted[\'my_trusted\']}": }')
       bolt_request.merge!({'options' => { 'compile_for_plan' => true } })
       expect {
         Puppet::Server::ASTCompiler.compile(bolt_request, boltlib_path)
       }.to raise_error(Puppet::Error, /the path to boltlib modules must be provided/)
     end
 
-    context 'when compiling for a plan with fact and plan/target variable collisions' do
+    context 'when compiling for an environment plan with fact and plan/target variable collisions' do
       let(:boltlib_path) { [] }
 
       it 'properly shadows the relevant variables' do
@@ -111,7 +125,7 @@ notify { "plan_var_fact": message => $plan_var_fact }
 notify { "target_var_fact": message => $target_var_fact }
 notify { "plan_var_plain":  message => $plan_var_plain }
 CODE
-        bolt_request = request(code)
+        bolt_request = environment_request(code)
         bolt_request.merge!(
           'facts'            => facts,
           'variables'        => variables,
@@ -132,6 +146,42 @@ CODE
           notify = find_notify(catalog, title: var)
           expect(notify).not_to be_nil
           expect(notify['parameters']['message']).to eql(expected_value)
+        end
+      end
+    end
+
+    context 'when running a project plan' do
+      let(:boltlib_path) { [] }
+      context 'single threaded' do
+        it 'returns puppet settings back to normal' do
+          # TODO: This is a temporary stub of the Bolt requires to get this
+          # test to pass. We should figure out how to properly load Bolt code
+          # later for the tests.
+          allow(Puppet::Server::ASTCompiler).to receive(:load_bolt)
+          mock_apply_inventory_class = double('Bolt::ApplyInventory')
+          allow(mock_apply_inventory_class).to receive(:new).with(anything).and_return(double('mock'))
+          stub_const("Bolt::ApplyInventory", mock_apply_inventory_class)
+
+          original_settings = {
+            'vardir' => Puppet[:vardir],
+            'confdir' => Puppet[:confdir],
+            'logdir' => Puppet[:logdir],
+            'codedir' => Puppet[:codedir],
+            'basemodulepath' => Puppet[:basemodulepath],
+            'vendormoduledir' => Puppet[:vendormoduledir],
+            'hiera_config' => Puppet[:hiera_config]
+          }
+
+          # Use a simple resource, since we aren't testing the compilation itself
+          # but rather ensuring the compilation does not interfere with global state
+          response = Puppet::Server::ASTCompiler.compile(project_request("notify { 'my_notify': }"), boltlib_path)
+          notify = find_notify(response[:catalog])
+          expect(notify).not_to be_nil
+          expect(notify['title']).to eq("my_notify")
+
+          original_settings.each do |setting_name, original_setting|
+            expect(Puppet[setting_name]).to eq(original_setting)
+          end
         end
       end
     end
