8fc80dbc257e111a982cd4cd523eb2cfb544341a
Maggie Dreyer <maggie@puppet.com>
2021-10-08 14:50:13 -0700

    (SERVER-3079) Install Dropsonde gem with package
    This commit causes the Dropsonde gem to be installed into an isolated
    directory during package install. This gem is meant to be run via MRI
    Ruby from the CLI, and uses its own self-contained dependencies, not
    gems installed with either puppetserver or puppet-agent.

--- resources/ext/build-scripts/install-vendored-gems.sh
+++ resources/ext/build-scripts/install-vendored-gems.sh
@@ -3,6 +3,21 @@
 set -x
 set -e
 
+install_gems () {
+  gem_file=$1
+  additional_args=$2
+
+  gem_list=()
+  while read LINE
+  do
+    gem_name=$(echo $LINE |awk '{print $1}')
+    gem_version=$(echo $LINE |awk '{print $2}')
+    gem_list+=("$gem_name:$gem_version")
+  done < $gem_file
+
+  java -cp puppet-server-release.jar:jruby-9k.jar clojure.main -m puppetlabs.puppetserver.cli.gem --config jruby.conf -- install ${additional_args:+"$additional_args"} --no-document "${gem_list[@]}"
+}
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -16,16 +31,9 @@ cat "${DIR}/jruby-gem-list.txt"
 
 echo "jruby-puppet: { gem-home: ${DESTDIR}/opt/puppetlabs/server/data/puppetserver/vendored-jruby-gems }" > jruby.conf
 
-gem_list=()
-while read LINE
-do
-  gem_name=$(echo $LINE |awk '{print $1}')
-  gem_version=$(echo $LINE |awk '{print $2}')
-  gem_list+=("$gem_name:$gem_version")
-done < "${DIR}/jruby-gem-list.txt"
-java -cp puppet-server-release.jar:jruby-9k.jar clojure.main -m puppetlabs.puppetserver.cli.gem --config jruby.conf -- install --no-document "${gem_list[@]}"
+install_gems "${DIR}/jruby-gem-list.txt"
 
-# We need to ignore dependencies to prevent puppetserver from being installed
+# We need to ignore dependencies to prevent puppetserver-ca from being installed
 # with facter2 (from gem dependency resolution) and facter3 (from puppet-agent packages)
 # If puppetserver ever loses its dependency on puppet-agent or if puppet-agent ever loses
 # facter, this will probably explode.
@@ -40,11 +48,14 @@ cat "${DIR}/mri-gem-list-no-dependencies.txt"
 
 echo "jruby-puppet: { gem-home: ${DESTDIR}/opt/puppetlabs/puppet/lib/ruby/vendor_gems }" > jruby.conf
 
-gem_list=()
-while read LINE
-do
-  gem_name=$(echo $LINE |awk '{print $1}')
-  gem_version=$(echo $LINE |awk '{print $2}')
-  gem_list+=("$gem_name:$gem_version")
-done < "${DIR}/mri-gem-list-no-dependencies.txt"
-java -cp puppet-server-release.jar:jruby-9k.jar clojure.main -m puppetlabs.puppetserver.cli.gem --config jruby.conf -- install --no-document --ignore-dependencies "${gem_list[@]}"
+install_gems "${DIR}/mri-gem-list-no-dependencies.txt" "--ignore-dependencies"
+
+# We need to install Dropsonde into an isolated directory so that its dependencies
+# can be installed with it without relying on the gems otherwise shipped by
+# puppet-agent or by puppetserver.
+echo "Installing Dropsonde into an isolated directory, to prevent dependency conflicts"
+cat "${DIR}/dropsonde-gem.txt"
+
+echo "jruby-puppet: { gem-home: ${DESTDIR}/opt/puppetlabs/server/data/puppetserver/dropsonde }" > jruby.conf
+
+install_gems "${DIR}/dropsonde-gem.txt"
