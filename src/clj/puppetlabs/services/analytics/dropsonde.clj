ea4954fbd008b8182f68dabb8d4efc51526a8d9e
Maggie Dreyer <maggie@puppet.com>
2021-10-25 11:10:29 -0700

    (SERVER-3079) Use server values for Puppet settings when running dropsonde
    This commit overrides the default values for various base Puppet
    settings, to prevent dropsonde from calculating them for a non-root
    user. The values now match what the rest of the server uses.

--- src/clj/puppetlabs/services/analytics/dropsonde.clj
+++ src/clj/puppetlabs/services/analytics/dropsonde.clj
@@ -1,18 +1,31 @@
 (ns puppetlabs.services.analytics.dropsonde
   (:require [clojure.java.shell :refer [sh]]
             [clojure.tools.logging :as log]
-            [puppetlabs.i18n.core :as i18n]))
+            [puppetlabs.i18n.core :as i18n]
+            [puppetlabs.services.jruby.jruby-puppet-core :as jruby-puppet]))
 
 (def puppet-agent-ruby "/opt/puppetlabs/puppet/bin/ruby")
 (def dropsonde-dir "/opt/puppetlabs/server/data/puppetserver/dropsonde")
 (def dropsonde-bin (str dropsonde-dir "/bin/dropsonde"))
 
 (defn run-dropsonde
-  []
-  (let [result (sh puppet-agent-ruby dropsonde-bin "submit"
+  [config]
+  (let [confdir (get-in config [:jruby-puppet :master-conf-dir]
+                        jruby-puppet/default-master-conf-dir)
+        codedir (get-in config [:jruby-puppet :master-code-dir]
+                        jruby-puppet/default-master-code-dir)
+        vardir (get-in config [:jruby-puppet :master-var-dir]
+                       jruby-puppet/default-master-var-dir)
+        logdir (get-in config [:jruby-puppet :master-log-dir]
+                       jruby-puppet/default-master-log-dir)
+        result (sh puppet-agent-ruby dropsonde-bin "submit"
                    :env {"GEM_HOME" dropsonde-dir
                          "GEM_PATH" dropsonde-dir
-                         "HOME" dropsonde-dir})]
+                         "HOME" dropsonde-dir
+                         "PUPPET_CONFDIF" confdir
+                         "PUPPET_CODEDIR" codedir
+                         "PUPPET_VARDIR" vardir
+                         "PUPPET_LOGDIR" logdir})]
     (if (= 0 (:exit result))
       (log/info (i18n/trs "Successfully submitted module metrics via Dropsonde."))
       (log/warn (i18n/trs "Failed to submit module metrics via Dropsonde. Error: {0}"
