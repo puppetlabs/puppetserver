0575d9dc96a06e1571b7fa22a49edb177af11ea3
Jonathan Albrecht <jonathan.albrecht@ibm.com>
2022-08-09 12:48:11 -0400

    (maint) Add the s390x arch to the tests
    Add the s390x arch but allow failures so it doesn't affect the
    build result. Note that jobs.include requires explicitly adding
    arch jobs for arch's other than the default amd64 arch.

--- ext/travisci/install-java.sh
+++ ext/travisci/install-java.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-echo "Installing Java $JAVA_VERSION"
+
+echo "Installing Java $JAVA_VERSION on arch $TRAVIS_CPU_ARCH"
 
 sudo rm -rf /usr/local/lib/jvm/
 sudo rm -rf /usr/lib/jvm/openjdk-$JAVA_VERSION
 sudo apt-get update
 sudo apt-get install -y openjdk-$JAVA_VERSION-jdk-headless
-export JAVA_HOME=/usr/lib/jvm/java-$JAVA_VERSION-openjdk-amd64/
+export JAVA_HOME=/usr/lib/jvm/java-$JAVA_VERSION-openjdk-$TRAVIS_CPU_ARCH/
