0fec15eaf2fb30d907ae171b9fe30e30855641f9
Justin Stoller <justin.stoller@gmail.com>
2022-09-08 15:49:28 -0700

    (maint) Use SnakeYAML 1.31 to resolve potential issues with CVEs

--- project.clj
+++ project.clj
@@ -34,7 +34,7 @@
 
                  [slingshot]
                  [clj-commons/clj-yaml]
-                 [org.yaml/snakeyaml]
+                 [org.yaml/snakeyaml "1.31"]
                  [commons-lang]
                  [commons-io]
 
