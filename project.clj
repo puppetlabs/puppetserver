ca9677ad0e6da56e784ad2e26516a69f5dd8275c
Justin Stoller <justin.stoller@gmail.com>
2023-02-13 15:58:51 -0800

    (SERVER-3167) Update to JRuby 9.4.1.0

--- project.clj
+++ project.clj
@@ -51,7 +51,7 @@
                  ;; send their logs to logstash, so we include it in the jar.
                  [net.logstash.logback/logstash-logback-encoder]
 
-                 [puppetlabs/jruby-deps "9.4.0.0-1"]
+                 [puppetlabs/jruby-deps "9.4.1.0-1"]
                  [puppetlabs/jruby-utils :exclusions [puppetlabs/jruby-deps]]
                  [puppetlabs/clj-shell-utils]
                  [puppetlabs/trapperkeeper]
