b28bd5dcaa23524370eeeb7e91ea872d97ed7708
Justin Stoller <justin.stoller@gmail.com>
2024-02-16 14:36:16 -0800

    Revert "(maint) update submodule versions and agent pin"
    This reverts commit d395a310e38c2848bcb696e868a3e0183929b0b5.

--- acceptance/config/beaker/options.rb
+++ acceptance/config/beaker/options.rb
@@ -11,5 +11,5 @@
  "puppetserver-confdir"=>"/etc/puppetlabs/puppetserver/conf.d",
  "puppetserver-config"=>
   "/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf",
- :puppet_build_version=>"c74b270d86dcf992e40cd04e820464c41ce53c85",
+ :puppet_build_version=>"4a645f3d010d32f30df4c015944d68a7bba425b2",
  :ssh=>{:config=>true}}
