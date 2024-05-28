c870bf66212248a2d37c99202a0fd0293ec43b8a
Jenkins CI <ci@puppet.com>
2024-05-28 23:59:28 +0000

    Set clj-parent=5.6.19

--- project.clj
+++ project.clj
@@ -30,7 +30,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "5.6.18"]
+  :parent-project {:coords [puppetlabs/clj-parent "5.6.19"]
                    :inherit [:managed-dependencies]}
 
   :dependencies [[org.clojure/clojure]
