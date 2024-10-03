6b4b2a20485f0a06be2a0a42309a73701a3cbb88
Justin Stoller <justin.stoller@gmail.com>
2024-10-02 21:41:18 -0700

    (maint) Update Compat tests for Beaker update
    This was missed in aeec8a3db.

--- acceptance/suites/puppet5_tests/puppet5_version_test.rb
+++ acceptance/suites/puppet5_tests/puppet5_version_test.rb
@@ -4,13 +4,13 @@ test_name "Testing Master/Agent backwards compatibility"
 legacy_agents = agents.reject { |agent| agent == master }
 
 step "Check that legacy agents have Puppet 5.x installed"
-on(legacy_agents, puppet("--version")) do
-  assert_match(/\A5\./, stdout, "puppet --version does not start with major version 5.")
+on(legacy_agents, puppet("--version")) do |result|
+  assert_match(/\A5\./, result.stdout, "puppet --version does not start with major version 5.")
 end
 
 step "Check that Puppet Server has Puppet 7.x installed"
-on(master, puppet("--version")) do
-  assert_match(/\A7/, stdout, "puppet --version does not start with major version 7.x")
+on(master, puppet("--version")) do |result|
+  assert_match(/\A7/, result.stdout, "puppet --version does not start with major version 8.x")
 end
 
 step "Check that the agent on the master runs against the master"
